576156a73e98b02e4ae8e823fca53938390ce833
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-05-26 11:47:08 +0000

    Added virtual machine task.

---
+++ openmole/plugins/org.openmole.plugin.task.systemexectask/src/main/java/org/openmole/plugin/task/systemexectask/VirtualSystemExecTask.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2010 Romain Reuillon <romain.reuillon at openmole.org>
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.task.systemexectask;
+
+import org.openmole.core.workflow.implementation.task.Task;
+import org.openmole.core.workflow.model.execution.IProgress;
+import org.openmole.core.workflow.model.job.IContext;
+import org.openmole.core.workflow.model.mole.IExecutionContext;
+import org.openmole.commons.exception.InternalProcessingError;
+import org.openmole.commons.exception.UserBadDataError;
+
+/**
+ *
+ * @author Romain Reuillon <romain.reuillon at openmole.org>
+ */
+public class VirtualSystemExecTask extends Task {
+
+    public VirtualSystemExecTask(String name) throws UserBadDataError, InternalProcessingError {
+        super(name);
+    }
+
+
+    @Override
+    protected void process(IContext context, IExecutionContext executionContext, IProgress progress) throws UserBadDataError, InternalProcessingError, InterruptedException {
+
+    }
+
+}
