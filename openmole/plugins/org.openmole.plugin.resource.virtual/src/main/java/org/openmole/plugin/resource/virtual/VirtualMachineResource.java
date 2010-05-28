a585c64089b198b72a2780157110a7e95c8cb635
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-05-28 17:27:30 +0000

    Enable static weaving.

--- openmole/plugins/org.openmole.plugin.resource.virtual/src/main/java/org/openmole/plugin/resource/virtual/VirtualMachineResource.java
+++ openmole/plugins/org.openmole.plugin.resource.virtual/src/main/java/org/openmole/plugin/resource/virtual/VirtualMachineResource.java
@@ -26,7 +26,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.logging.Logger;
 import org.apache.commons.exec.CommandLine;
