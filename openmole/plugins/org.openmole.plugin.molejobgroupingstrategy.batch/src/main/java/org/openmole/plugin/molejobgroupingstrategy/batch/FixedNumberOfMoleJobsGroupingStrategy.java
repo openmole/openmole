0f59d6269390161847cded3fad50fd7681d1a94d
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-05-29 11:19:20 +0000

    Renamed batch grouping strategy package.

--- openmole/plugins/org.openmole.plugin.molejobgroupingstrategy.batch/src/main/java/org/openmole/plugin/molejobgroupingstrategy/batch/FixedNumberOfMoleJobsGroupingStrategy.java
+++ openmole/plugins/org.openmole.plugin.molejobgroupingstrategy.batch/src/main/java/org/openmole/plugin/molejobgroupingstrategy/batch/FixedNumberOfMoleJobsGroupingStrategy.java
@@ -1,21 +1,21 @@
 /*
- *  Copyright (C) 2010 reuillon
- *
+ *  Copyright (C) 2010 Romain Reuillon <romain.reuillon at openmole.org>
+ * 
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *
+ * 
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
+ * 
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.openmole.plugin.molejobgroupingstrategy.batchgroupingstrategy;
 
+package org.openmole.plugin.molejobgroupingstrategy.batch;
 
 import org.openmole.core.workflow.implementation.mole.MoleJobCategory;
 import org.openmole.core.workflow.model.execution.IMoleJobCategory;
@@ -26,23 +26,33 @@ import org.openmole.commons.exception.UserBadDataError;
 
 /**
  *
- * @author Romain Reuillon <romain.reuillon@openmole.fr>
+ * @author Romain Reuillon <romain.reuillon at openmole.org>
  */
-public class FixedNumberOfBatchGroupingStrategy implements IMoleJobGroupingStrategy {
+public class FixedNumberOfMoleJobsGroupingStrategy implements IMoleJobGroupingStrategy {
 
-    final private int batchSize;
+    final private int numberOfMoleJobs;
     private Integer currentBatchNumber = 0;
+    private Integer currentNumberOfJobs = 0;
 
-    public FixedNumberOfBatchGroupingStrategy(int batchSize) {
-        this.batchSize = batchSize;
+    public FixedNumberOfMoleJobsGroupingStrategy(int numberOfMoleJobs) {
+        this.numberOfMoleJobs = numberOfMoleJobs;
     }
 
     @Override
     public IMoleJobCategory getCategory(IContext context) throws InternalProcessingError, UserBadDataError {
-        Object[] tab = {currentBatchNumber};
-        IMoleJobCategory jobCategory = new MoleJobCategory(tab);
-        currentBatchNumber = (currentBatchNumber+1)%batchSize;
-        return jobCategory;
+        Object[] args = {currentBatchNumber};
+        IMoleJobCategory ret = new MoleJobCategory(args);
 
+        currentNumberOfJobs++;
+        if(currentNumberOfJobs >= numberOfMoleJobs) {
+            currentNumberOfJobs = 0;
+            currentBatchNumber++;
+        }
+
+        return ret;
     }
+
+
+
+
 }
