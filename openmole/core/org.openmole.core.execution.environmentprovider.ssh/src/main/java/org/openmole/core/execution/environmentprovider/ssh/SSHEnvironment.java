7ec0642322965fc30f2062b1c301df2deb98f93f
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-05-29 11:59:06 +0000

    Revert "Removed ssh environment."
    This reverts commit 2328dabd9286a571a3ea0810ce259ca6e157ef54.

---
+++ openmole/core/org.openmole.core.execution.environmentprovider.ssh/src/main/java/org/openmole/core/execution/environmentprovider/ssh/SSHEnvironment.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openmole.core.environmentprovider.ssh;
+
+import java.util.Collection;
+import org.openmole.commons.exception.InternalProcessingError;
+import org.openmole.commons.exception.UserBadDataError;
+import org.openmole.misc.workspace.ConfigurationLocation;
+import org.openmole.core.environmentprovider.jsaga.JSAGAExecutionEnvironment;
+import org.openmole.core.environmentprovider.jsaga.model.IJSAGAJobService;
+import org.openmole.core.environmentprovider.jsaga.model.IJSAGALaunchingScript;
+import org.openmole.core.environmentprovider.ssh.internal.Activator;
+import org.openmole.core.workflow.model.execution.EnvironmentConfiguration;
+import org.openmole.core.workflow.model.execution.batch.IBatchStorage;
+
+/**
+ *
+ * @author reuillon
+ */
+public class SSHEnvironment extends JSAGAExecutionEnvironment<SSHEnvironmentDescription> {
+
+    final static String ConfigGroup = SSHEnvironment.class.getSimpleName();
+    final static ConfigurationLocation DefaultNbSpot = new ConfigurationLocation(ConfigGroup, "DefaultNbSpot");
+
+    static {
+        Activator.getWorkspace().addToConfigurations(DefaultNbSpot, "4");
+    }
+
+
+    Integer nbSpots;
+
+    public SSHEnvironment(SSHEnvironmentDescription description) throws InternalProcessingError {
+        super(description);
+        nbSpots = Activator.getWorkspace().getPreferenceAsInt(DefaultNbSpot);
+    }
+
+    public void setNbSpots(Integer nbSpots) {
+        this.nbSpots = nbSpots;
+    }
+
+  
+
+    @Override
+    protected Collection<IJSAGAJobService> allJobServices() throws InternalProcessingError, UserBadDataError {
+
+
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected Collection<IBatchStorage> allStorages() throws InternalProcessingError, UserBadDataError {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setConfigurationMode(EnvironmentConfiguration configuration) throws InternalProcessingError {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public IJSAGALaunchingScript<?> getLaunchingScript() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void initializeAccess() throws UserBadDataError, InternalProcessingError, InterruptedException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
