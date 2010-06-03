a2c6ff23ba4c14f65cbcaec74929375237e47f7d
Nicolas Dumoulin <nicolas.dumoulin@cemagref.fr>
2010-06-03 09:05:33 +0000

    * weaving dependencies removed  * some duplicated dependencies not copyed in product

--- openmole/ui/openmole-ui-console/org.openmole.ui.console/src/main/resources/run.sh
+++ openmole/ui/openmole-ui-console/org.openmole.ui.console/src/main/resources/run.sh
@@ -10,5 +10,6 @@ fi
 
 rm -Rf configuration/org*
 rm configuration/*.log
-java -Xmx${MEM} -jar plugins/org.eclipse.equinox.launcher_1.0.200.jar -p openmole-plugins,openmole-plugins-ui $@ -vmargs -Dosgi.framework.extensions=org.eclipse.equinox.weaving.hook
+java -Xmx${MEM} -jar plugins/org.eclipse.equinox.launcher_1.0.200.jar\
+     -p openmole-plugins,openmole-plugins-ui $@
 cat configuration/*.log
