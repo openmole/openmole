d5864a0bec28d6da4ac36dbf0ac096ba6be23429
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-06-30 08:40:01 +0000

    Integration of J3D for visualisation.

---
+++ openmole/plugins/org.openmole.plugin.task.visualisation3d/src/main/java/org/openmole/plugin/task/visualisation3d/BarChart3D.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2010 reuillon
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.task.visualisation3d;
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import org.openmole.commons.exception.InternalProcessingError;
+import org.openmole.commons.exception.UserBadDataError;
+import org.openmole.core.implementation.task.Task;
+import org.openmole.core.model.execution.IProgress;
+import org.openmole.core.model.job.IContext;
+
+/**
+ *
+ * @author reuillon
+ */
+public class BarChart3D extends Task {
+
+    public BarChart3D(String name) throws UserBadDataError, InternalProcessingError {
+        super(name);
+    }
+    
+    
+    @Override
+    protected void process(IContext context, IProgress progress) throws UserBadDataError, InternalProcessingError, InterruptedException {
+        
+    }
+
+}
