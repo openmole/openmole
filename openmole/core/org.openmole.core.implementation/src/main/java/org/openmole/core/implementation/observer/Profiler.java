379d6b0d0b4ea9492921882d339449c138edcfe8
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-08-13 23:35:06 +0200

    Add Affero clause.

--- openmole/core/org.openmole.core.implementation/src/main/java/org/openmole/core/implementation/observer/Profiler.java
+++ openmole/core/org.openmole.core.implementation/src/main/java/org/openmole/core/implementation/observer/Profiler.java
@@ -4,7 +4,7 @@ package org.openmole.core.implementation.observer;
  *  Copyright (C) 2010 Romain Reuillon <romain.reuillon at openmole.org>
  * 
  *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
+ *  it under the terms of the Affero GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  * 
