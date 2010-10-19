c57b8de5db937cc617f1f5ed000176b9ff789d40
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-10-19 08:11:49 +0200

    Added missing files.

---
+++ openmole/core/org.openmole.core.implementation/src/main/java/org/openmole/core/implementation/sampling/Sampling.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (C) 2010 reuillon
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.openmole.core.implementation.sampling;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openmole.core.model.sampling.IFactor;
+import org.openmole.core.model.sampling.ISampling;
+
+public abstract class Sampling<T extends IFactor<?, ?>> implements ISampling {
+
+    final private List<T> factors = new ArrayList<T>();
+
+    public Sampling() {
+    }
+
+    public Sampling(T... factors) {
+        for (T f : factors) {
+            addFactor(f);
+        }
+    }
+
+    public void addFactor(T e) {
+        factors.add(e);
+    }
+
+    public List<T> getFactors() {
+        return factors;
+    }
+
+}
