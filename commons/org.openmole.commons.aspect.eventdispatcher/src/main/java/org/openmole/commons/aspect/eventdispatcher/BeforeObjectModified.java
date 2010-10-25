839b1913bbb4d3b659788bd3a5112715c9b40e35
Romain Reuillon <romain.reuillon@iscpif.fr>
2010-10-25 18:10:03 +0200

    Modified type -> name

--- commons/org.openmole.commons.aspect.eventdispatcher/src/main/java/org/openmole/commons/aspect/eventdispatcher/BeforeObjectModified.java
+++ commons/org.openmole.commons.aspect.eventdispatcher/src/main/java/org/openmole/commons/aspect/eventdispatcher/BeforeObjectModified.java
@@ -29,5 +29,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface BeforeObjectModified {
-    String type();
+    String name();
 }
