56febe568095f2c7f1a9cdf29d83aa955c1b8ad1
Romain Reuillon <romain.reuillon@iscpif.fr>
2011-05-04 15:02:46 +0200

    Added scala iterable variable domain.

---
+++ openmole/plugins/org.openmole.plugin.domain.collection/src/main/scala/org/openmole/plugin/domain/collection/ScalaIterableVariableDomain.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2011 reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.domain.collection
+
+import org.openmole.core.model.data.IContext
+import org.openmole.core.model.data.IPrototype
+import org.openmole.core.model.domain.IDomain
+import org.openmole.misc.exception.UserBadDataError
+
+class ScalaIterableVariableDomain[T](variable: IPrototype[Iterable[_ <: T]]) extends IDomain[T] {
+
+  override def iterator(context: IContext): Iterator[T] = 
+    context.value(variable).getOrElse(throw new UserBadDataError("Value not found in context for prototype " + variable.toString())).iterator  
+
+}
