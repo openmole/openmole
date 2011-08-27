2f0ef5be78df9c9ebe017ece5d189a2eb58d58ed
leclaire <mathieu.leclaire@iscpif.fr>
2011-08-27 19:31:12 +0200

    Customize piecharts

--- platform/misc/org.openmole.ide.misc.visualization/src/main/scala/org/openmole/ide/misc/visualization/PiePlotter.scala
+++ platform/misc/org.openmole.ide.misc.visualization/src/main/scala/org/openmole/ide/misc/visualization/PiePlotter.scala
@@ -17,19 +17,43 @@
 
 package org.openmole.ide.misc.visualization
 
-import java.lang.Comparable
+import java.awt.Color
+import java.awt.Dimension
 import org.jfree.chart.ChartFactory
-import org.jfree.data.general.DefaultPieDataset
 import org.jfree.chart.ChartPanel
-import org.jfree.chart.JFreeChart
+import org.jfree.chart.plot.PiePlot
+import org.jfree.data.general.DefaultPieDataset
 
-class PiePlotter(title: String, data: Map[String, Integer]) {
-  def this(title: String) = this(title,Map.empty[String,Integer])
+class PiePlotter(title: String, data: Map[String, Double]) {
+  def this(title: String) = this(title,Map.empty[String,Double])
 
   val pieData = new DefaultPieDataset
-  val chart = ChartFactory.createPieChart(title,pieData , false, true, false)
+  data.foreach(d=>pieData.setValue(d._1,d._2))
+  
+  val chart = ChartFactory.createPieChart(title,pieData , false, false, false)
+  customize(chart.getPlot.asInstanceOf[PiePlot])
   
   def updateData(key: String, value: Double) = pieData.setValue(key,value)
   
-  def chartPanel = new ChartPanel(chart)
+  def chartPanel = new ChartPanel(chart) {setPreferredSize(new Dimension(250,250))}
+  
+  private def customize(plot: PiePlot) = {
+    chart.getTitle.setPaint(new Color(102,102,102))
+    chart.setAntiAlias(true)
+    plot.setShadowPaint(new Color(0,0,0,0))
+    plot.setBackgroundPaint(new Color(0,0,0,0))
+    plot.setLabelBackgroundPaint(new Color(0,0,0,0))
+    plot.setLabelOutlinePaint(new Color(0,0,0,0))
+    plot.setLabelShadowPaint(new Color(0,0,0,0))
+    plot.setLabelLinksVisible(false)
+    plot.setIgnoreZeroValues(true)
+    plot.setLabelPaint(new Color(102,102,102))
+    
+    plot.setSectionPaint("Ready",new Color(255,204,0))
+    plot.setSectionPaint("Running",new Color(85,212,0))
+    plot.setSectionPaint("Submitted",new Color(198,233,175))
+    plot.setSectionPaint("Failed",new Color(255,255,255))
+    plot.setSectionPaint("Killed",new Color(170,0,0))
+    plot.setSectionPaint("Done",new Color(0,170,212))   
+  }
 }
