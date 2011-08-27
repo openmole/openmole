2192567c58d7b68eb752db33a38a530244a6060e
leclaire <mathieu.leclaire@iscpif.fr>
2011-08-27 19:29:54 +0200

    Implement desktopgrid environment

---
+++ platform/plugins/org.openmole.ide.plugin.environment.desktopgrid/src/main/scala/org/openmole/ide/plugin/environment/desktopgrid/DesktopGridEnvironmentPanelUI.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.plugin.environment.desktopgrid
+
+import java.text.NumberFormat
+import org.openmole.ide.core.model.panel.IEnvironmentPanelUI
+import org.openmole.ide.misc.widget.MigPanel
+import scala.swing.FormattedTextField
+import scala.swing.Label
+import scala.swing.TextField
+import scala.swing.PasswordField
+
+class DesktopGridEnvironmentPanelUI(pud: DesktopGridEnvironmentDataUI) extends MigPanel("fillx,wrap 2","[left][grow,fill]","") with IEnvironmentPanelUI{
+  val loginTextField = new TextField(20)
+  val passTextField = new PasswordField(20)
+  val portTextField = new FormattedTextField(NumberFormat.getIntegerInstance)
+  
+  contents+= (new Label("Login"),"gap para")
+  contents+= loginTextField
+  contents+= (new Label("Password"),"gap para")
+  contents+= passTextField
+  contents+= (new Label("Port"),"gap para")
+  contents+= portTextField
+  
+  loginTextField.text = pud.login
+  passTextField.text = pud.pass
+  portTextField.text = pud.port.toString
+  
+  override def saveContent(name: String) = new DesktopGridEnvironmentDataUI(name,
+                                                                      loginTextField.text,
+                                                                      passTextField.text,
+                                                                      portTextField.text.filterNot(_==' ').toInt)
+}
