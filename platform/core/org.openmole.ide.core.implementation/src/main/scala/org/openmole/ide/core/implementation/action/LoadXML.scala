a281bfefadd7625d9161f6bf1b8802ae4e268c8c
Mathieu leclaire <mathieu.leclaire@iscpif.fr>
2011-08-29 12:56:16 +0200

    Adds load and save classes

---
+++ platform/core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/action/LoadXML.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.core.implementation.action
+
+import java.io.File
+import javax.swing.filechooser.FileNameExtensionFilter
+import scala.swing.FileChooser.SelectionMode._
+import org.openmole.ide.core.implementation.serializer.GUISerializer
+import scala.swing.FileChooser.Result.Approve
+import scala.swing.Component
+import scala.swing.FileChooser
+import scala.swing.Label
+
+object LoadXML {
+  
+  def show = {
+    val fc = new FileChooser {
+      new FileNameExtensionFilter("Select an xml file to be loaded", ".xml,.XML")
+      fileSelectionMode = FilesOnly
+      title =" OpenMOLE project loading"
+    }
+  
+    var text=""
+    if (fc.showDialog(new Label,"OK") == Approve) text = fc.selectedFile.getPath
+    if (new File(text).isFile) GUISerializer.unserialize(text)
+  }
+}
---
+++ platform/core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/action/SaveXML.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.core.implementation.action
+
+import java.io.File
+import javax.swing.filechooser.FileNameExtensionFilter
+import scala.swing.FileChooser.SelectionMode._
+import org.openmole.ide.core.implementation.control.Settings
+import org.openmole.ide.core.implementation.serializer.GUISerializer
+import scala.swing.FileChooser.Result.Approve
+import scala.swing.Component
+import scala.swing.FileChooser
+import scala.swing.Label
+
+object SaveXML {
+  def save = GUISerializer.serialize(Settings.currentProject.getOrElse(show))
+  
+def show : String = {
+    val fc = new FileChooser {
+      new FileNameExtensionFilter("Save", ".xml,.XML")
+      fileSelectionMode = FilesOnly
+      title ="Save OpenMOLE project"
+    }
+  
+    var text=""
+    if (fc.showDialog(new Label,"OK") == Approve) text = fc.selectedFile.getPath
+    println("Parent " + new File(text).getParentFile)
+    println("save : " + text.split('.')(0)+".xml")
+    if (new File(text).getParentFile.isDirectory()) text.split('.')(0)+".xml"
+    text
+  }
+}
