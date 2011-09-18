1a5106cd7a86ff6589e0021b4d0e4b888783f7f2
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2011-09-18 19:50:18 +0200

    Fix Bug

--- platform/plugins/org.openmole.ide.plugin.domain.distribution/src/main/scala/org/openmole/ide/plugin/domain/distribution/SlicedUniformIntegerDistributionDataUI.scala
+++ platform/plugins/org.openmole.ide.plugin.domain.distribution/src/main/scala/org/openmole/ide/plugin/domain/distribution/SlicedUniformIntegerDistributionDataUI.scala
@@ -7,13 +7,13 @@ package org.openmole.ide.plugin.domain.distribution
 
 import java.io.File
 import org.openmole.ide.core.model.data.IDomainDataUI
-import org.openmole.plugin.domain.distribution.SlicedUniformIntegerDistribution
+import org.openmole.plugin.domain.distribution.SlicedUniformIntDistribution
 
 class SlicedUniformIntegerDistributionDataUI(val name: String,val seed: Long=0,val size: Int=1) extends IDomainDataUI[Int] {
   
-  override def coreObject = new SlicedUniformIntegerDistribution(seed,size)
+  override def coreObject = new SlicedUniformIntDistribution(seed,size)
 
-  override def coreClass = classOf[SlicedUniformIntegerDistribution] 
+  override def coreClass = classOf[SlicedUniformIntDistribution] 
   
   override def imagePath = "img/domain_uniform_distribution.png"
   
