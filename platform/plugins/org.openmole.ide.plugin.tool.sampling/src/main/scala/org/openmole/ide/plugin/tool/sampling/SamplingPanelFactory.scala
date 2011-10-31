f24a00290fc57344ff08a1cdb7d075f4613d172e
Mathieu leclaire <mathieu.leclaire@iscpif.fr>
2011-10-31 10:03:22 +0100

    first stuff for mixing domains and samplings

---
+++ platform/plugins/org.openmole.ide.plugin.tool.sampling/src/main/scala/org/openmole/ide/plugin/tool/sampling/SamplingPanelFactory.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.plugin.tool.sampling
+
+import org.openmole.ide.core.implementation.dataproxy.DomainDataProxyFactory
+import org.openmole.ide.core.model.factory.IDomainFactoryUI
+import scala.collection.JavaConversions._
+import java.util.HashSet
+import org.openide.util.Lookup
+
+object SamplingPanelFactory {
+  val modelDomains = new HashSet[DomainDataProxyFactory]
+  Lookup.getDefault.lookupAll(classOf[IDomainFactoryUI[_]]).foreach(f=>modelDomains += new DomainDataProxyFactory(f))
+  
+  def sampleDomainPanel= "to be implemented"
+  
+  def sampleSamplingPanel= "to be implemented"
+}
