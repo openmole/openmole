ecf41290259fb4c53854f54d1791c719564e00da
Mathieu Leclaire <mathieu.leclaire@openmole.org>
2012-03-13 16:38:31 +0100

    fix core api change

--- platform/plugins/org.openmole.ide.plugin.sampling.combine/src/main/scala/org/openmole/ide/plugin/sampling/combine/CompleteSamplingDataUI.scala
+++ platform/plugins/org.openmole.ide.plugin.sampling.combine/src/main/scala/org/openmole/ide/plugin/sampling/combine/CompleteSamplingDataUI.scala
@@ -9,19 +9,19 @@ import org.openmole.ide.core.model.dataproxy._
 import org.openmole.ide.core.model.data.IDomainDataUI
 import org.openmole.ide.core.model.data.ISamplingDataUI
 import org.openmole.plugin.sampling.combine.CompleteSampling
-import org.openmole.core.implementation.sampling.Factor
+import org.openmole.core.implementation.sampling.DiscreteFactor
 import org.openmole.core.model.data.IPrototype
 import org.openmole.core.model.domain.IDomain
+import org.openmole.core.model.domain.IIterable
 import org.openmole.ide.core.implementation.data.EmptyDataUIs._
 import scala.collection.JavaConversions._
 
 class CompleteSamplingDataUI(val name: String="", 
                              val factors: List[(IPrototypeDataProxyUI ,String,IDomainDataUI)] = List.empty) extends ISamplingDataUI {
 
-  def coreObject = new CompleteSampling(factors.map(f=>new Factor(
+  def coreObject = new CompleteSampling(factors.map(f=>new DiscreteFactor(
         f._1.dataUI.coreObject.asInstanceOf[IPrototype[Any]],
-      //  DomainDataProxyFactory.factoryByName(f._2).buildDataProxyUI(name).dataUI.coreObject)))
-      f._3.coreObject(f._1.dataUI.coreObject).asInstanceOf[IDomain[Any]])))
+      f._3.coreObject(f._1.dataUI.coreObject).asInstanceOf[IDomain[Any] with IIterable[Any]])))
 
   def coreClass = classOf[CompleteSampling] 
   
