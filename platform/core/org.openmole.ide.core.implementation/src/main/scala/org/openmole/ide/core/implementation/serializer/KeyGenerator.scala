fc5499d1c4570ea02626e7f3f77255d53c5ce870
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2012-03-27 00:30:10 +0200

    Key referencement for core object identification

---
+++ platform/core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/serializer/KeyGenerator.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 mathieu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.core.implementation.serializer
+
+import org.openmole.core.model.data.IPrototype
+import org.openmole.misc.tools.obj.ClassUtils._
+
+
+object KeyGenerator {
+  def stripArrays(m : Manifest[_]) : Manifest[_] = {
+    if (m.erasure.isArray) stripArrays(m.erasure.fromArray.toManifest)
+    else m
+  }
+  
+  def apply(proto : IPrototype[_]) : Key = {
+    println("XXXXXXXXXXXXXXXx :: " + stripArrays(proto.`type`))
+    new PrototypeKey(proto.getClass,stripArrays(proto.`type`))
+  }
+}
