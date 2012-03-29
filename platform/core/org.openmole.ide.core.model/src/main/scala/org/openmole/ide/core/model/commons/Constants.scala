c35efe66e80e463dac0a949c220fda163065ae43
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2012-03-29 10:22:29 +0200

    Remode unlesse global variables

--- platform/core/org.openmole.ide.core.model/src/main/scala/org/openmole/ide/core/model/commons/Constants.scala
+++ platform/core/org.openmole.ide.core.model/src/main/scala/org/openmole/ide/core/model/commons/Constants.scala
@@ -16,46 +16,13 @@
  */
 package org.openmole.ide.core.model.commons
 
-import java.awt.Color
-import java.awt.Toolkit
-import java.awt.datatransfer.DataFlavor
-
 object Constants{
   val MOVE= "move"
   val SELECT = "connect"
   val CONNECT = "connect"
   val RECONNECT = "connect"
   
-  val TASK_HEADER_BACKGROUND_COLOR= new Color(68, 120, 33)
-  val TASK_SELECTION_COLOR= new Color(255, 100, 0)
-  val CONNECTOR_LABEL_BACKGROUND_COLOR= new Color(255, 246, 213)
-  val CONNECTOR_LABEL_BORDER_COLOR= new Color(255, 204, 0)
-  val DATA_CHANNEL_LABEL_BACKGROUND_COLOR= new Color(215, 238, 244)
-  val DATA_CHANNEL_LABEL_BORDER_COLOR= new Color(55, 113, 200)
-  val CONNECTOR_LABEL_FONT_COLOR= new Color(0, 68, 85)
-  val DEFAULT_BACKGROUND_TASK_COLOR= new Color(255,255,255)
-  val DEFAULT_BORDER_TASK_COLOR= new Color(0,0,0)
-  
-  val SCREEN_WIDTH= Toolkit.getDefaultToolkit.getScreenSize.width
-  val SCREEN_HEIGHT= Toolkit.getDefaultToolkit.getScreenSize.height
-  val PANEL_WIDTH= SCREEN_WIDTH* 0.8
-  val PANEL_HEIGHT= SCREEN_HEIGHT* 0.8
-  val EXPANDED_TASK_CONTAINER_WIDTH= 200
   val TASK_CONTAINER_WIDTH= 100
   val TASK_CONTAINER_HEIGHT= 120
-  val TASK_TITLE_WIDTH = TASK_CONTAINER_WIDTH
   val TASK_TITLE_HEIGHT= 20
-  val TASK_IMAGE_HEIGHT = TASK_CONTAINER_HEIGHT - TASK_TITLE_HEIGHT - 20
-  val TASK_IMAGE_WIDTH= 70
-  val TASK_IMAGE_HEIGHT_OFFSET= TASK_TITLE_HEIGHT + 10
-  val TASK_IMAGE_WIDTH_OFFSET= (TASK_CONTAINER_WIDTH - TASK_IMAGE_WIDTH) / 2
-  val EXPANDED_TASK_IMAGE_WIDTH_OFFSET= (EXPANDED_TASK_CONTAINER_WIDTH - TASK_IMAGE_WIDTH) / 2
-  val NB_MAX_SLOTS= 5
-    
-  val TASK = "TASK"
-  val PROTOTYPE = "PROTOTYPE"
-  val SAMPLING = "SAMPLING"
-  val ENVIRONMENT = "ENVIRONMENT"
-  val DOMAIN = "DOMAIN"
-  val HOOK = "HOOK"
 }
