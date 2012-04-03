8da31eec4def25e5a58697ef5334c4daa080521b
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-04-03 14:31:12 +0200

    Moved ui to the upper package.

---
+++ ui/org.openmole.ui.console/src/main/resources/debug.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+MEM="512m"
+
+if [ $# -gt 0 ]
+then
+	MEM=$1	
+fi
+
+rm -Rf configuration/org*
+rm configuration/*.log
+java -Xdebug -Xnoagent \
+  -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n \
+  -Xmx${MEM} -jar plugins/org.eclipse.equinox.launcher_1.1.0.jar\
+  -p openmole-plugins,openmole-plugins-ui $@
+cat configuration/*.log
