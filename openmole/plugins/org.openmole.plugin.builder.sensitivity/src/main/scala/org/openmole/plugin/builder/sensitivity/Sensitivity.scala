7ecdeb058542597748d132316d5f74029a4e9e80
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-04-03 14:17:05 +0200

    Create plugin for sensitivity builder.

---
+++ openmole/plugins/org.openmole.plugin.builder.sensitivity/src/main/scala/org/openmole/plugin/builder/sensitivity/Sensitivity.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2012 reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.builder.sensitivity
+
+import org.openmole.core.implementation.mole.Capsule
+import org.openmole.core.implementation.mole.StrainerCapsule
+import org.openmole.core.implementation.puzzle.PuzzleFirstAndLast
+import org.openmole.core.implementation.sampling.DiscreteFactor
+import org.openmole.core.implementation.task.EmptyTask
+import org.openmole.core.implementation.task.ExplorationTask
+import org.openmole.core.implementation.transition.AggregationTransition
+import org.openmole.core.implementation.transition.ExplorationTransition
+import org.openmole.core.implementation.transition.Transition
+import org.openmole.core.implementation.data.DataChannel
+import org.openmole.core.implementation.data.Prototype
+import org.openmole.core.model.IPuzzleFirstAndLast
+import org.openmole.core.model.data.IPrototype
+import org.openmole.core.model.mole.ICapsule
+import org.openmole.plugin.builder.Builder._
+import org.openmole.plugin.method.sensitivity.SaltelliSampling
+
+object Sensitivity {
+  
+}
