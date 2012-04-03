422d77ffb47eb10baf9843ecc47de5176753b71b
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2012-04-03 14:21:40 +0200

    Implement Bounded Domain

---
+++ platform/core/org.openmole.ide.core.model/src/main/scala/org/openmole/ide/core/model/data/IBoundedDomainDataUI.scala
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.openmole.ide.core.model.data
+
+import org.openmole.core.model.data.IPrototype
+import org.openmole.core.model.domain.IBounded
+import org.openmole.core.model.domain.IDomain
+import org.openmole.ide.core.model.panel.IBoundedDomainPanelUI
+
+trait IBoundedDomainDataUI extends IDataUI {
+  
+  def coreObject(proto: IPrototype[Double]) : IDomain[Double] with IBounded[Double]
+  
+  def buildPanelUI: IBoundedDomainPanelUI
+}
---
+++ platform/core/org.openmole.ide.core.model/src/main/scala/org/openmole/ide/core/model/panel/IBoundedDomainPanelUI.scala
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.openmole.ide.core.model.panel
+
+import org.openmole.ide.core.model.data.IBoundedDomainDataUI
+
+trait IBoundedDomainPanelUI extends IPanelUI{
+  def saveContent(name: String): IBoundedDomainDataUI
+}
