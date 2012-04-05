bd66dea2d7dc930276cd0baf2944ca5773e670b5
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-04-05 16:56:07 +0200

    Added AppendArrayToFileHook.

--- openmole/plugins/org.openmole.plugin.hook.file/src/main/scala/org/openmole/plugin/hook/file/AppendArrayToFileHook.scala
+++ openmole/plugins/org.openmole.plugin.hook.file/src/main/scala/org/openmole/plugin/hook/file/AppendArrayToFileHook.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 reuillon
+ * Copyright (C) 2012 reuillon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,20 +24,20 @@ import org.openmole.core.model.data.IPrototype
 import org.openmole.core.model.job.IMoleJob
 import org.openmole.core.model.mole.ICapsule
 import org.openmole.core.model.mole.IMoleExecution
-import org.openmole.misc.exception.UserBadDataError
 import org.openmole.misc.tools.io.FileUtil._
 
-class AppendToFileHook(
+class AppendArrayToFileHook(
   moleExecution: IMoleExecution,
   capsule: ICapsule,
   fileName: String,
-  content: String) extends CapsuleExecutionHook(moleExecution, capsule) {
+  content: IPrototype[Array[_]]) extends CapsuleExecutionHook(moleExecution, capsule) {
   
   override def process(moleJob: IMoleJob) = {
     import moleJob.context
     val file = new File(VariableExpansion.expandData(context,fileName))
     file.createParentDir
-    file.lockApply(_.append(VariableExpansion.expandData(context,content)))
+    val toWrite = context.value(content).getOrElse(Array("not found")).mkString(",")
+    file.lockApply(_.appendLine(toWrite))
   }
   
 }
