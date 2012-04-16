5f89cadc6bdb9c2815b39708f4b9ffaf7d5bbf45
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-04-17 00:48:49 +0200

    Compilation fix.

--- openmole/plugins/org.openmole.plugin.grouping.batch/src/main/scala/org/openmole/plugin/grouping/batch/package.scala
+++ openmole/plugins/org.openmole.plugin.grouping.batch/src/main/scala/org/openmole/plugin/grouping/batch/package.scala
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.openmole.plugin.groupingstrategy
+package org.openmole.plugin.grouping
 
 import org.openmole.core.implementation.puzzle.Puzzle
 
