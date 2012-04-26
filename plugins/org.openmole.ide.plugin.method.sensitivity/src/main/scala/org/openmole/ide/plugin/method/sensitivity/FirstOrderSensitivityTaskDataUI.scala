cd74a3d5397f502ff47ce136a1146e93cc41f87e
Mathieu Leclaire <mathieu.leclaire@openmole.org>
2012-04-26 20:18:35 +0200

    Add sensitivity method files

---
+++ plugins/org.openmole.ide.plugin.method.sensitivity/src/main/scala/org/openmole/ide/plugin/method/sensitivity/FirstOrderSensitivityTaskDataUI.scala
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.openmole.ide.plugin.method.sensitivity
+
+import java.awt.Color
+import org.openmole.core.model.data.IDataSet
+import org.openmole.core.model.data.IParameterSet
+import org.openmole.core.model.data.IPrototype
+import org.openmole.core.model.task.IPluginSet
+import org.openmole.ide.core.implementation.data.TaskDataUI
+
+class FirstOrderSensitivityTaskDataUI(val name: String="")
+                                      //val matrixName: IPrototype[String],
+                                    //  val modelInputs: Iterable[IPrototype[Double]],
+                                    //  val modelOutputs: Iterable[IPrototype[Double]]) extends TaskDataUI {
+  
+//  def coreObject(inputs: IDataSet, outputs: IDataSet, parameters: IParameterSet, plugins: IPluginSet) = {
+//    val builder = 
+//      builder addInput inputs
+//    builder addOutput outputs
+//    builder addParameter parameters
+//    builder.toTask
+//  }
+//  
+//  def coreClass= classOf[FirstOrderSensitivityTask]
+//  
+//  def fatImagePath = "img/groovyTask_fat.png"
+//  
+//  def buildPanelUI = new FirstOrderSensitivityTaskPanelUI(this)
+//  
+//  def borderColor = new Color(61,104,130)
+//  
+//  def backgroundColor = new Color(61,104,130,128)
+//}
---
+++ plugins/org.openmole.ide.plugin.method.sensitivity/src/main/scala/org/openmole/ide/plugin/method/sensitivity/FirstOrderSensitivityTaskPanelUI.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.plugin.method.sensitivity
+
+import java.awt.Dimension
+import org.openmole.ide.core.model.data.ITaskDataUI
+import org.openmole.ide.core.model.panel.ITaskPanelUI
+import scala.swing.FileChooser.SelectionMode._
+import org.openmole.ide.misc.widget.Help
+import org.openmole.ide.misc.widget.PluginPanel
+import scala.swing.Label
+
+class FirstOrderSensitivityTaskPanelUI(pud: GroovyTaskDataUI) extends PluginPanel("fillx,wrap","[left,grow,fill]","") with ITaskPanelUI {
+  
+  
+  override def saveContent(name: String):ITaskDataUI = new FirstOrderSensitivityTaskDataUI(name)
+}
