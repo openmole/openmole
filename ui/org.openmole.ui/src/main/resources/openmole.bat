65988df79c48443276c8324e51f9ba948ec1d73b
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-05-02 19:52:36 +0200

    Added uninstall icons and short cut pannel. Corrected windows launcher.

--- ui/org.openmole.ui/src/main/resources/openmole.bat
+++ ui/org.openmole.ui/src/main/resources/openmole.bat
@@ -2,4 +2,5 @@ rmdir /s /q "configuration\org.eclipse.core.runtime"
 rmdir /s /q "configuration\org.eclipse.equinox.app"
 rmdir /s /q "configuration\org.eclipse.osgi"
 
-java -ea -XX:MaxPermSize=128M -XX:+UseParallelGC -Xmx1G -jar plugins\org.eclipse.equinox.launcher.jar -p openmole-plugins -g openmole-plugins-gui %*
+java -ea -XX:MaxPermSize=128M -XX:+UseParallelGC -Xmx1G  -XX:MaxPermSize=128M -XX:+UseParallelGC -jar ./plugins/org.eclipse.equinox.launcher.jar -cp ./openmole-plugins -gp ./openmole-plugins-gui %*
+
