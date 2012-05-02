51b6e8066f0935c66e0edbf805c14e2394429e63
Mathieu Leclaire <mathieu.leclaire@openmole.org>
2012-05-02 17:53:44 +0200

    move STAT_TASK in TASK

--- core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/panel/ComponentCategories.scala
+++ core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/panel/ComponentCategories.scala
@@ -19,14 +19,16 @@ package org.openmole.ide.core.implementation.panel
 
 object ComponentCategories {
   
-  val ABM_TASK = new ComponentCategory("agent based model")
+  val ABM_TASK = new ComponentCategory("Agent based model")
   
-  val SENSITIVITY_TASK = new ComponentCategory("sensitivity",
+  val SENSITIVITY_TASK = new ComponentCategory("Sensitivity",
                                                List(new ComponentCategory("Saltelli")))
   
-  val STORAGE_TASK = new ComponentCategory("storage")
+  val STORAGE_TASK = new ComponentCategory("Storage")
+  
+  val STAT_TASK = new ComponentCategory("Stat")
            
-  val TASK = new ComponentCategory("New",List(ABM_TASK,SENSITIVITY_TASK,STORAGE_TASK))
+  val TASK = new ComponentCategory("New",List(ABM_TASK,SENSITIVITY_TASK,STORAGE_TASK,STAT_TASK))
   
   val PROTOTYPE = new ComponentCategory("New")
   
