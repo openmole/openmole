6b34bcba06ed7ecc44dec21162c486e1d597fb2c
Mathieu Leclaire <mathieu.leclaire@openmole.org>
2012-06-07 15:18:16 +0200

    remove implicit serialization

--- core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/data/TaskDataUI.scala
+++ core/org.openmole.ide.core.implementation/src/main/scala/org/openmole/ide/core/implementation/data/TaskDataUI.scala
@@ -27,8 +27,8 @@ abstract class TaskDataUI extends ITaskDataUI {
   var inputParameters: scala.collection.mutable.Map[IPrototypeDataProxyUI, String] = HashMap.empty[IPrototypeDataProxyUI, String]
   var prototypesIn = List.empty[IPrototypeDataProxyUI]
   var prototypesOut = List.empty[IPrototypeDataProxyUI]
-  var implicitPrototypesIn: List[IPrototypeDataProxyUI] = List.empty[IPrototypeDataProxyUI]
-  var implicitPrototypesOut: List[IPrototypeDataProxyUI] = List.empty[IPrototypeDataProxyUI]
+  @transient var implicitPrototypesIn: List[IPrototypeDataProxyUI] = List.empty[IPrototypeDataProxyUI]
+  @transient var implicitPrototypesOut: List[IPrototypeDataProxyUI] = List.empty[IPrototypeDataProxyUI]
 
   def filterPrototypeOccurencies(pproxy: IPrototypeDataProxyUI) =
     (prototypesIn.filter(_ == pproxy) ++ prototypesOut.filter(_ == pproxy)).distinct
