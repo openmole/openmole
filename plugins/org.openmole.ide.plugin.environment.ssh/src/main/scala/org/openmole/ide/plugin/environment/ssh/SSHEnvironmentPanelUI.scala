42e5d1ff791ba115c48b5f33d5275c2c90bfd529
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2012-06-27 20:01:24 +0200

    Implements ssh plugin

---
+++ plugins/org.openmole.ide.plugin.environment.ssh/src/main/scala/org/openmole/ide/plugin/environment/ssh/SSHEnvironmentPanelUI.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2011 <mathieu.leclaire at openmole.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.ide.plugin.environment.ssh
+
+import org.openmole.ide.core.model.panel.IEnvironmentPanelUI
+import org.openmole.ide.misc.widget.PluginPanel
+import scala.swing.Label
+import scala.swing.TextField
+
+class SSHEnvironmentPanelUI(pud: SSHEnvironmentDataUI) extends PluginPanel("fillx,wrap 2", "", "") with IEnvironmentPanelUI {
+
+  val loginTextField = new TextField(pud.login)
+  val hostTextField = new TextField(pud.host)
+  val nbSlotTextField = new TextField(pud.nbSlots.toString, 3)
+  val dirTextField = new TextField(pud.dir)
+  val runTimeMemoryTextField = new TextField(pud.runtimeMemory)
+
+  contents += (new Label("Login"), "gap para")
+  contents += loginTextField
+
+  contents += (new Label("Host"), "gap para")
+  contents += hostTextField
+
+  contents += (new Label("Number of slots"), "gap para")
+  contents += nbSlotTextField
+
+  contents += (new Label("Directory"), "gap para")
+  contents += dirTextField
+
+  contents += (new Label("Runtime memory"), "gap para")
+  contents += runTimeMemoryTextField
+
+  override def saveContent(name: String) = new SSHEnvironmentDataUI(name,
+    loginTextField.text,
+    hostTextField.text,
+    nbSlotTextField.text.toInt,
+    dirTextField.text,
+    runTimeMemoryTextField.text.toInt)
+}
