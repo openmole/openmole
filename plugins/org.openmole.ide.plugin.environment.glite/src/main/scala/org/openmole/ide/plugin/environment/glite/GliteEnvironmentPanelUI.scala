11761e1f72991851efdc60cc5016d9ebb4fdc5f6
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2012-07-03 18:23:51 +0200

    All generic requirement panel

--- plugins/org.openmole.ide.plugin.environment.glite/src/main/scala/org/openmole/ide/plugin/environment/glite/GliteEnvironmentPanelUI.scala
+++ plugins/org.openmole.ide.plugin.environment.glite/src/main/scala/org/openmole/ide/plugin/environment/glite/GliteEnvironmentPanelUI.scala
@@ -19,12 +19,13 @@ package org.openmole.ide.plugin.environment.glite
 
 import java.util.Locale
 import java.util.ResourceBundle
+import org.openmole.ide.plugins.misc.RequirementPanelUI
 import org.openmole.ide.core.model.panel.IEnvironmentPanelUI
 import org.openmole.ide.misc.widget._
 import scala.swing.CheckBox
 import scala.swing.Label
+import scala.swing.TabbedPane
 import scala.swing.TextField
-import scala.swing.event.ButtonClicked
 
 class GliteEnvironmentPanelUI(pud: GliteEnvironmentDataUI) extends PluginPanel("fillx", "[left][grow,fill]", "") with IEnvironmentPanelUI {
 
@@ -32,116 +33,71 @@ class GliteEnvironmentPanelUI(pud: GliteEnvironmentDataUI) extends PluginPanel("
 
   val voTextField = new TextField(20) {
     tooltip = Help.tooltip(i18n.getString("vo"),
-      i18n.getString("voEx"))
+                           i18n.getString("voEx"))
   }
   val vomsTextField = new TextField(20) {
     tooltip = Help.tooltip(i18n.getString("voms"),
-      i18n.getString("vomsEx"))
+                           i18n.getString("vomsEx"))
   }
   val bdiiTextField = new TextField(20) {
     tooltip = Help.tooltip(i18n.getString("bdii"),
-      i18n.getString("bdiiEx"))
+                           i18n.getString("bdiiEx"))
   }
 
   val proxyCheckBox = new CheckBox("MyProxy") { tooltip = Help.tooltip(i18n.getString("myProxy")) }
   val proxyURLTextField = new TextField(18) {
     tooltip = Help.tooltip(i18n.getString("proxyURL"),
-      i18n.getString("proxyURLEx"))
+                           i18n.getString("proxyURLEx"))
   }
   val proxyURLLabel = new Label("url")
 
-  val requirementCheckBox = new CheckBox("Requirements") { tooltip = Help.tooltip(i18n.getString("requirement")) }
-  val architectureCheckBox = new CheckBox("64 bits") { tooltip = Help.tooltip(i18n.getString("64bits")) }
+  val requirements = new RequirementPanelUI(pud.architecture64,
+                                            pud.runtimeMemory,
+                                            pud.workerNodeMemory,
+                                            pud.maxCPUTime,
+                                            pud.otherRequirements)
+  
+  val tabbedPane = new TabbedPane
+  tabbedPane.pages += new TabbedPane.Page("Requirements",
+                                          new PluginPanel("wrap 2") {
+      contents += (new Label("VO"), "gap para")
+      contents += voTextField
+      contents += (new Label("VOMS"), "gap para")
+      contents += vomsTextField
+      contents += (new Label("BDII"), "gap para")
+      contents += bdiiTextField
+      contents += (proxyCheckBox, "wrap")
+      contents += (proxyURLLabel, "gap para")
+      contents += proxyURLTextField
+    })
 
-  val runtimeMemoryLabel = new Label("RuntimeMemory")
-  val runtimeMemoryTextField = new TextField(4) {
-    tooltip = Help.tooltip(i18n.getString("runtimeMemory"),
-      i18n.getString("runtimeMemoryEx"))
-  }
-  val workerNodeMemoryLabel = new Label("Worker memory")
-  val workerNodeMemoryTextField = new TextField(4) {
-    tooltip = Help.tooltip(i18n.getString("workerNodeMemory"),
-      i18n.getString("workerNodeMemoryEx"))
-  }
-  val maxCPUTimeLabel = new Label("Max CPU Time")
-  val maxCPUTimeTextField = new TextField(4) {
-    tooltip = Help.tooltip(i18n.getString("maxCPUTime"),
-      i18n.getString("maxCPUTimeEx"))
-  }
-  val otherRequirementLabel = new Label("Other")
-  val otherRequirementTextField = new TextField(16) { tooltip = Help.tooltip(i18n.getString("other")) }
-
-  contents += (new PluginPanel("wrap 2") {
-    contents += (new Label("VO"), "gap para")
-    contents += voTextField
-    contents += (new Label("VOMS"), "gap para")
-    contents += vomsTextField
-    contents += (new Label("BDII"), "gap para")
-    contents += bdiiTextField
-  }, "wrap")
-  contents += (new PluginPanel("wrap 2") {
-    contents += (proxyCheckBox, "wrap")
-    contents += (proxyURLLabel, "gap para")
-    contents += proxyURLTextField
-  }, "wrap")
-  contents += (new PluginPanel("wrap 2") {
-    contents += (requirementCheckBox, "wrap")
-    contents += (architectureCheckBox, "wrap")
-    contents += (runtimeMemoryLabel, "gap para")
-    contents += runtimeMemoryTextField
-    contents += (workerNodeMemoryLabel, "gap para")
-    contents += workerNodeMemoryTextField
-    contents += (maxCPUTimeLabel, "gap para")
-    contents += maxCPUTimeTextField
-    contents += otherRequirementLabel
-    contents += otherRequirementTextField
-  }, "wrap")
-
+  tabbedPane.pages += requirements
+  
   voTextField.text = pud.vo
   vomsTextField.text = pud.voms
   bdiiTextField.text = pud.bdii
   proxyURLTextField.text = pud.proxyURL
   proxyCheckBox.selected = pud.proxy
-  requirementCheckBox.selected = pud.requirement
-  architectureCheckBox.selected = pud.architecture64
-  runtimeMemoryTextField.text = pud.runtimeMemory
-  workerNodeMemoryTextField.text = pud.workerNodeMemory
-  maxCPUTimeTextField.text = pud.maxCPUTime
   showProxy(pud.proxy)
-  showRequirements(pud.requirement)
-  otherRequirementTextField.text = pud.otherRequirements
 
-  listenTo(`proxyCheckBox`, `requirementCheckBox`)
-  reactions += {
-    case ButtonClicked(`requirementCheckBox`) ⇒ showRequirements(requirementCheckBox.selected)
-    case ButtonClicked(`proxyCheckBox`) ⇒ showProxy(proxyCheckBox.selected)
-  }
-
+  contents += tabbedPane
+  
   private def showProxy(b: Boolean) = {
     List(proxyURLLabel, proxyURLLabel, proxyURLTextField).foreach {
       _.visible = b
     }
   }
 
-  private def showRequirements(b: Boolean) = {
-    List(architectureCheckBox, runtimeMemoryLabel,
-      runtimeMemoryTextField, workerNodeMemoryLabel,
-      workerNodeMemoryTextField, maxCPUTimeLabel,
-      maxCPUTimeTextField, otherRequirementLabel, otherRequirementTextField).foreach { _.visible = b }
-  }
-
   override def saveContent(name: String) =
     new GliteEnvironmentDataUI(name,
-      voTextField.text,
-      vomsTextField.text,
-      bdiiTextField.text,
-      proxyCheckBox.selected,
-      proxyURLTextField.text,
-      requirementCheckBox.selected,
-      architectureCheckBox.selected,
-      runtimeMemoryTextField.text,
-      workerNodeMemoryTextField.text,
-      maxCPUTimeTextField.text,
-      otherRequirementTextField.text)
-
+                               voTextField.text,
+                               vomsTextField.text,
+                               bdiiTextField.text,
+                               proxyCheckBox.selected,
+                               proxyURLTextField.text,
+                               requirements.architectureCheckBox.selected,
+                               requirements.runtimeMemoryTextField.text,
+                               requirements.workerNodeMemoryTextField.text,
+                               requirements.maxCPUTimeTextField.text,
+                               requirements.otherRequirementTextField.text)
 }
