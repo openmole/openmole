cd2c83a3971c0a0a90170217eb536df830d7818f
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-07-27 09:50:16 +0200

    Separated osgi config folder for each run.

--- ui/org.openmole.ui/src/main/resources/openmole.bat
+++ ui/org.openmole.ui/src/main/resources/openmole.bat
@@ -4,5 +4,8 @@ rmdir /s /q "configuration\org.eclipse.osgi"
 
 start dbserver\bin\openmole-dbserver.bat
 
-java -Dosgi.classloader.singleThreadLoads=true -splash:splashscreen.png -XX:MaxPermSize=128M -XX:+UseParallelGC -Xmx1G  -XX:MaxPermSize=128M -XX:+UseParallelGC -jar ./plugins/org.eclipse.equinox.launcher.jar -cp ./openmole-plugins -gp ./openmole-plugins-gui %*
+set ran=%random%
 
+java -Dosgi.classloader.singleThreadLoads=true -Dosgi.configuration.area=%ran% -splash:splashscreen.png -XX:MaxPermSize=128M -XX:+UseParallelGC -Xmx1G  -XX:MaxPermSize=128M -XX:+UseParallelGC -jar ./plugins/org.eclipse.equinox.launcher.jar -cp ./openmole-plugins -gp ./openmole-plugins-gui %*
+
+rmdir /s /q %ran%
