7879dc6e1e2a5ed41eba44ffa4358ba95462accf
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-08-27 15:27:50 +0200

    Implement select file domain.

---
+++ openmole/plugins/org.openmole.plugin.domain.file/src/main/scala/org/openmole/plugin/domain/file/SelectFileDomain.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.domain.file
+
+import java.io.File
+import org.openmole.core.model.data._
+import org.openmole.core.model.domain._
+import org.openmole.core.implementation.tools._
+
+class SelectFileDomain(val base: File, val path: String) extends IDomain[File] with IFinite[File] {
+
+  override def computeValues(context: IContext): Iterable[File] =
+    List(new File(base, VariableExpansion(context, path)))
+
+}
--- openmole/plugins/org.openmole.plugin.task.code/src/main/scala/org/openmole/plugin/task/code/CodeTaskBuilder.scala
+++ openmole/plugins/org.openmole.plugin.task.code/src/main/scala/org/openmole/plugin/task/code/CodeTaskBuilder.scala
@@ -21,7 +21,8 @@ import java.io.File
 import org.openmole.plugin.task.external.ExternalTaskBuilder
 import scala.collection.mutable.ListBuffer
 
-/** Builder for any code task 
+/**
+ * Builder for any code task
  *
  * The code task builder is an external task builder, you may want to look
  * at the @see ExternalTaskBuilder for a complement of documentation.
@@ -33,27 +34,29 @@ abstract class CodeTaskBuilder extends ExternalTaskBuilder {
   def imports = _imports.toList
   def libraries = _libraries.toList
 
-  /** Add a namespace import and make it available to in the task
+  /**
+   * Add a namespace import and make it available to in the task
    *
    * For instance addImport("java.io.*") in a groovy task make the content of the
    * java.io package available in the groovy code.
    *
-   * @param s a namespace   
+   * @param s a namespace
    */
   def addImport(s: String) = {
     _imports += s
     this
   }
 
-  /** Add a library and make it available to the task
-   * 
+  /**
+   * Add a library and make it available to the task
+   *
    * For instance addLib("/tmp/malib.jar") in a groovy task make the content of the
    * jar available to the task. This method support jars but has some limitation. The
    * best way to use your own bytecode (java, scala, groovy, jython) in OpenMOLE is
-   * building an OpenMOLE plugin (see the section on openmole.org for details). 
-   * 
+   * building an OpenMOLE plugin (see the section on openmole.org for details).
+   *
    * @param l a jar file
-   * 
+   *
    */
   def addLib(l: File) = {
     _libraries += l
