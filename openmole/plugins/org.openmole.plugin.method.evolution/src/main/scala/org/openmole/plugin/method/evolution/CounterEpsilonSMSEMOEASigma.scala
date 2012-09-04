5b958e4fe76b568b6991d1c2b2a52bd3641b4a37
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-09-04 18:21:56 +0200

    Add algo with counter termination.

---
+++ openmole/plugins/org.openmole.plugin.method.evolution/src/main/scala/org/openmole/plugin/method/evolution/CounterEpsilonSMSEMOEASigma.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2012 reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.method.evolution
+
+import fr.iscpif.mgo._
+
+sealed class CounterEpsilonSMSEMOEASigma(
+  val distributionIndex: Double,
+  val steps: Int,
+  val genomeSize: Int,
+  val mu: Int,
+  val lambda: Int,
+  val epsilons: Seq[Double],
+  val referencePoint: Seq[Double]) extends NSGAIISigma
+    with BinaryTournamentSelection
+    with CounterTermination
+    with NonDominatedElitism
+    with CoEvolvingSigmaValuesMutation
+    with SBXBoundedCrossover
+    with HypervolumeDiversity
+    with ParetoRanking
+    with EpsilonDominance
+    with RankDiversityModifier
+    with EvolutionManifest
+    with TerminationManifest {
+
+  val gManifest = manifest[G]
+  val individualManifest = manifest[Individual[G]]
+  val populationManifest = manifest[Population[G, MF]]
+  val stateManifest = manifest[STATE]
+
+}
