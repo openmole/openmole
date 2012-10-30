e56524885fa9b82e1242e9c9e468a113111df945
Romain Reuillon <romain.reuillon@iscpif.fr>
2012-10-30 10:45:55 +0100

    Compilation fix.

--- openmole/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteEnvironment.scala
+++ openmole/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteEnvironment.scala
@@ -113,7 +113,7 @@ class GliteEnvironment(
     val jobType: Option[String] = None,
     val smpGranularity: Option[Int] = None,
     val myProxy: Option[MyProxy] = None,
-    val def architecture: Option[String] = None) extends BatchEnvironment with MemoryRequirement { env ⇒
+    val architecture: Option[String] = None) extends BatchEnvironment with MemoryRequirement { env ⇒
 
   import GliteEnvironment._
 
