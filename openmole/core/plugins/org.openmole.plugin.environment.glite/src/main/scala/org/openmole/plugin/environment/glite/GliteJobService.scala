ba7e1aec78a2310438d64838fabd5e34d1c9e617
Romain Reuillon <romain.reuillon@iscpif.fr>
2013-06-10 12:53:24 +0200

    Update gridscale and start refactoring glite to include DIRAC.

--- openmole/core/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteJobService.scala
+++ openmole/core/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteJobService.scala
@@ -45,7 +45,7 @@ object GliteJobService extends Logger {
 
 import GliteJobService._
 
-trait GliteJobService extends GridScaleJobService with JobServiceQualityControl with LimitedAccess with AvailabitityQuality { js ⇒
+trait GliteJobService extends GridScaleJobService with JobServiceQualityControl with LimitedAccess with AvailabitityQuality with JobScript { js ⇒
 
   val jobService: WMSJobService
   def environment: GliteEnvironment
@@ -97,79 +97,6 @@ trait GliteJobService extends GridScaleJobService with JobServiceQualityControl 
     }
     finally script.delete
   }
-
-  protected def generateScript(
-    serializedJob: SerializedJob,
-    resultPath: String,
-    runningPath: String,
-    finishedPath: String,
-    os: OutputStream) = {
-    import serializedJob._
-
-    val writter = new PrintStream(os)
-
-    assert(runtime.runtime.path != null)
-
-    writter.print(touch(storage.url.resolve(runningPath)))
-    writter.print("; BASEPATH=$PWD; CUR=$PWD/ws$RANDOM; while test -e $CUR; do CUR=$PWD/ws$RANDOM;done;mkdir $CUR; export HOME=$CUR; cd $CUR; export OPENMOLE_HOME=$CUR; ")
-    writter.print("if [ `uname -m` = x86_64 ]; then ")
-    writter.print(lcgCpGunZipCmd(storage.url.resolve(runtime.jvmLinuxX64.path), "$PWD/jvm.tar.gz")) //, homeCacheDir, runtime.jvmLinuxX64.hash))
-    writter.print("; else ")
-    writter.print(lcgCpGunZipCmd(storage.url.resolve(runtime.jvmLinuxI386.path), "$PWD/jvm.tar.gz")) //, homeCacheDir, runtime.jvmLinuxI386.hash))
-    writter.print("; fi; ")
-    writter.print("tar -xzf jvm.tar.gz >/dev/null; rm -f jvm.tar.gz; ")
-    writter.print(lcgCpGunZipCmd(storage.url.resolve(runtime.runtime.path), "$PWD/openmole.tar.gz")) //, homeCacheDir, runtime.runtime.hash))
-    writter.print("; tar -xzf openmole.tar.gz >/dev/null; rm -f openmole.tar.gz; ")
-    writter.print("mkdir envplugins; PLUGIN=0;")
-
-    for (plugin ← runtime.environmentPlugins) {
-      assert(plugin.path != null)
-      writter.print(lcgCpGunZipCmd(storage.url.resolve(plugin.path), "$CUR/envplugins/plugin$PLUGIN.jar")) //, homeCacheDir, plugin.hash))
-      writter.print("; PLUGIN=`expr $PLUGIN + 1`; ")
-    }
-
-    writter.print(lcgCpCmd(storage.url.resolve(runtime.storage.path), "$CUR/storage.xml.gz"))
-
-    writter.print(" ; export PATH=$PWD/jre/bin:$PATH; /bin/sh run.sh ")
-    writter.print(environment.openMOLEMemoryValue)
-    writter.print("m ")
-    writter.print(UUID.randomUUID)
-    writter.print(" -c ")
-    writter.print(path)
-    writter.print(" -s $CUR/storage.xml.gz ")
-    writter.print(" -p $CUR/envplugins/ ")
-    writter.print(" -i ")
-    writter.print(inputFile)
-    writter.print(" -o ")
-    writter.print(resultPath)
-    writter.print(" -t ")
-    writter.print(environment.threadsValue)
-    writter.print("; ")
-    writter.print(touch(storage.url.resolve(finishedPath)))
-    writter.print("; cd .. ; rm -rf $CUR ; ")
-  }
-
-  protected def touch(dest: URI) = {
-    val name = UUID.randomUUID.toString
-    s"touch $name; ${lcgCpCmd(name, dest)}; rm $name "
-  }
-
-  protected def lcgCpGunZipCmd(from: URI, to: String) = {
-    val builder = new StringBuilder
-    builder.append(lcgCpCmd(from, to + ".gz"))
-    builder.append(" && gunzip ")
-    builder.append(to)
-    builder.append(".gz ")
-    builder.toString
-  }
-
-  @transient lazy val lcgCp =
-    s"lcg-cp --vo ${environment.voName} --checksum --connect-timeout $getTimeOut --sendreceive-timeout $getTimeOut --bdii-timeout $getTimeOut --srm-timeout $getTimeOut "
-
-  protected def lcgCpCmd(from: String, to: URI) = s"$lcgCp file:$from ${to.toString}"
-  protected def lcgCpCmd(from: URI, to: String) = s"$lcgCp ${from.toString} file:$to"
-
-  private def getTimeOut = Workspace.preferenceAsDuration(GliteEnvironment.RemoteTimeout).toSeconds.toString
 
   protected def buildJobDescription(runtime: Runtime, script: File) =
     new WMSJobDescription {
