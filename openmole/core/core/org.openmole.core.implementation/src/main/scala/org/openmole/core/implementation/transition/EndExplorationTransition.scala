ffdb4c973666215e0ae7a327b2a95df38db92d05
Romain Reuillon <romain.reuillon@iscpif.fr>
2013-06-21 06:41:33 +0200

    Cancel submole if an error occurs.

--- openmole/core/core/org.openmole.core.implementation/src/main/scala/org/openmole/core/implementation/transition/EndExplorationTransition.scala
+++ openmole/core/core/org.openmole.core.implementation/src/main/scala/org/openmole/core/implementation/transition/EndExplorationTransition.scala
@@ -17,23 +17,28 @@
 
 package org.openmole.core.implementation.transition
 
-import org.openmole.core.implementation.tools._
-import org.openmole.core.model.tools._
 import org.openmole.core.model.data._
 import org.openmole.core.model.mole._
 import org.openmole.core.model.transition.ICondition._
 import org.openmole.core.model.transition._
 import org.openmole.misc.exception._
 import org.openmole.misc.tools.service.LockUtil._
+import scala.util.{ Failure, Success, Try }
 
 class EndExplorationTransition(start: ICapsule, end: Slot, trigger: ICondition, filter: Filter[String] = Filter.empty) extends Transition(start, end, True, filter) with IEndExplorationTransition {
 
   override protected def _perform(context: Context, ticket: ITicket, subMole: ISubMoleExecution) = {
-    if (!subMole.canceled && trigger.evaluate(context)) {
+    def perform() {
       val parentTicket = ticket.parent.getOrElse(throw new UserBadDataError("End exploration transition should take place after an exploration."))
       val subMoleParent = subMole.parent.getOrElse(throw new InternalProcessingError("Submole execution has no parent"))
       subMoleParent.transitionLock { super._perform(context, parentTicket, subMoleParent) }
       subMole.cancel
+    }
+
+    Try(!subMole.canceled && trigger.evaluate(context)) match {
+      case Success(true) ⇒ perform()
+      case Failure(t)    ⇒ subMole.cancel; throw t
+      case _             ⇒
     }
   }
 
