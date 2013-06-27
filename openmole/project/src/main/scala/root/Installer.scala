a0bd8f24c179a4f48abeacf29fe36d97ddb5cc57
markhammons <markehammons@gmail.com>
2013-06-27 05:25:44 +0200

    Fixed the installer yaml. Beware of built-in variables that the yaml parser doesn't recognize!!

--- openmole/project/src/main/scala/root/Installer.scala
+++ openmole/project/src/main/scala/root/Installer.scala
@@ -9,7 +9,7 @@ import org.openmole.buildsystem.OMKeys._
 import Application.{ openmolePlugins, openmoleRuntime }
 
 object Installer extends Defaults {
-  val dir = file("installer")
+  val dir = file(".")
   val all = Aggregator("installer-all") aggregate (installer)
 
   lazy val openmoleSettings: Seq[Setting[_]] = openmolePlugins.settings
@@ -18,11 +18,11 @@ object Installer extends Defaults {
 
   lazy val t = target in openmolePlugins
 
-  lazy val installer = AssemblyProject("izpack-installer", "", settings = IzPack.izPackSettings ++ resAssemblyProject) settings (
+  lazy val installer = AssemblyProject("installer", "installer", settings = IzPack.izPackSettings ++ resAssemblyProject) settings (
     assemble := false,
     packageBin := file("."),
     createInstaller in IzPack.IzPack.Config <<= (createInstaller in IzPack.IzPack.Config) dependsOn resourceAssemble,
-    variables in Config <+= version { v ⇒ ("version", v) },
+    variables in Config <++= version { v ⇒ Seq(("version", v), "home" -> "$USER_HOME") },
     installSourceDir in Config <<= assemblyPath,
     configFile in Config <<= assemblyPath { _ / "resources/install.yml" },
     resourceSets <<= (assemblyPath in openmolePlugins, target in openmoleRuntime, tarGZName in openmoleRuntime, baseDirectory) map { (assembly, target, tarGz, bD) ⇒
