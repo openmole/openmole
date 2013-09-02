61fdfbb0dc5482d8024a4ce20d40043070be9314
Romain Reuillon <romain.reuillon@iscpif.fr>
2013-09-02 06:47:57 +0200

    Implement confidence interval task.

---
+++ openmole/core/plugins/org.openmole.plugin.task.stat/src/main/scala/org/openmole/plugin/task/stat/ConfidenceIntervalTask.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 02/09/13 Romain Reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openmole.plugin.task.stat
+
+import org.openmole.misc.tools.math.Stat
+import org.openmole.core.model.task.PluginSet
+
+object ConfidenceIntervalTask {
+
+  def apply(name: String, level: Double = 0.95)(implicit plugins: PluginSet) = new DoubleSequenceStatTaskBuilder { builder ⇒
+    def toTask = new ConfidenceIntervalTask(name, level) with builder.Built
+  }
+
+}
+
+abstract class ConfidenceIntervalTask(val name: String, val level: Double) extends DoubleSequenceStatTask {
+
+  override def stat(seq: Array[Double]) = Stat.confidenceInterval(seq, level)
+
+}
+
