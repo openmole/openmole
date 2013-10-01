33c73a7acf6eb25fb46254021112d538ec57d926
markhammons <markehammons@gmail.com>
2013-10-01 16:39:11 +0200

    Fixed mathieu's bug.

--- openmole/project/src/main/scala/root/Web.scala
+++ openmole/project/src/main/scala/root/Web.scala
@@ -25,8 +25,7 @@ object Web extends Defaults {
     imports = Seq("org.h2.*", "*;resolution:=optional")) dependsOn
     (h2, jetty, slick, logback, scalatra, bonecp, scalaLang, base.Core.implementation, base.Core.serializer, xstream, jacksonJson, iceTar, arm)
 
-  lazy val misc = OsgiProject("org.openmole.web.misc.tools", "misc/tools",
-    exports = Seq("org.openmole.web.misc.tools.*")) dependsOn
+  lazy val misc = OsgiProject("org.openmole.web.misc.tools", "misc/tools") dependsOn
     (scalajHttp)
 
   override def OsgiSettings = super.OsgiSettings ++ Seq(bundleType := Set("core"))
