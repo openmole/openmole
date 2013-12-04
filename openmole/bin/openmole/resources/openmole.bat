a88828c1a7704ad8e40826d174d9cb33a1d86c7b
Romain Reuillon <romain.reuillon@iscpif.fr>
2013-12-04 18:05:07 +0100

    Fix windows launching script.

--- openmole/bin/openmole/resources/openmole.bat
+++ openmole/bin/openmole/resources/openmole.bat
@@ -1,17 +1,10 @@
-
 set PWD=%~dp0
-
-start /MIN %PWD%\dbserver\bin\openmole-dbserver.bat
-
+start /MIN %PWD%dbserver\bin\openmole-dbserver.bat
 mkdir "%UserProfile%\.openmole\.tmp"
-
 set ran="%UserProfile%\.openmole\.tmp\%random%"
-
 java -d64 -version >nul 2>&1
 if errorlevel 1 goto is32bit
 set FLAG="-XX:+UseCompressedOops"
 :is32bit
-
-java -Dosgi.locking=none -Dopenmole.location="%PWD%\" -Dosgi.classloader.singleThreadLoads=true -Dosgi.configuration.area=%ran% -splash:splashscreen.png -XX:MaxPermSize=128M -XX:+UseG1GC -Xmx1G  -XX:MaxPermSize=128M %FLAG% -jar ./plugins/org.eclipse.equinox.launcher.jar -cp ./openmole-plugins -gp ./openmole-plugins-gui %*
-
+java -Dosgi.locking=none -Dopenmole.location="%PWD%\" -Dosgi.classloader.singleThreadLoads=true -Dosgi.configuration.area=%ran% -splash:splashscreen.png -XX:MaxPermSize=128M -XX:+UseG1GC -Xmx1G  -XX:MaxPermSize=128M %FLAG% -jar %PWD%/plugins/org.eclipse.equinox.launcher.jar -cp %PWD%/openmole-plugins -gp %PWD%/openmole-plugins-gui %*
 rmdir /s /q %ran%
