4fb4f8c42c5bb1da7b711a92dd4fb45e5ca23773
markhammons <markehammons@gmail.com>
2013-12-09 04:50:12 +0100

    ScalaClient no longer hangs on to the password after instantiation.

--- openmole/web/misc/tools/src/main/scala/org/openmole/web/misc/tools/RESTClient.scala
+++ openmole/web/misc/tools/src/main/scala/org/openmole/web/misc/tools/RESTClient.scala
@@ -20,23 +20,30 @@ import java.security.KeyStore
 
 //TODO: There's so much to fix in this class. Consult romain and mathieu about it.
 
-trait OMClientInterface {
+abstract class RESTClientInterface(pass: String) {
   def address: String
   def path: String
-  def pass: String
   def createMole(moleData: Array[Byte], context: Option[Array[Byte]] = None, pack: Boolean = false, encapsulate: Boolean = false): Any
   def getLoadedMoles: Any
   def getMoleStats(id: String): Any
   def startMole(id: String): Any
   def deleteMole(id: String): Any
   def trustCert(): Unit
-  def isCertTrusted: Boolean
-  def getCert: Certificate
+  def isCertTrusted: Option[Boolean]
+  def cert: Option[Certificate]
   val fullAddress = if (address.endsWith("/")) address + path else address + "/" + path
 }
 
-case class HTTPControls(address: String, path: String, pass: String) extends OMClientInterface {
-  lazy val apiKey = (Http.post(address + "/xml/getApiKey").header("pass", pass).option(HttpOptions.connTimeout(10000)).option(HttpOptions.readTimeout(10000)).asXml \ "apiKey").text
+class HTTPControls(val address: String, val path: String, pass: String) extends RESTClientInterface(pass) {
+  private val ks = KeyStoreTools.getOMInsecureKeyStore
+
+  private val hostnameVerifier: HostnameVerifier = new HostnameVerifier {
+    def verify(p1: String, p2: SSLSession): Boolean = true
+  }
+
+  private var sslFactory = genSSLFactory(ks)
+
+  val apiKey = (finishRequest(Http.post(address + "/xml/getApiKey").header("pass", pass)).asXml \ "apiKey").text
 
   def createMole(moleData: Array[Byte], context: Option[Array[Byte]], pack: Boolean = false, encapsulate: Boolean = false) = {
     val packVal = if (pack) "on" else ""
@@ -68,14 +75,6 @@ case class HTTPControls(address: String, path: String, pass: String) extends OMC
 
   private def finishRequest(h: Http.Request) = h.option(HttpOptions.sslSocketFactory(sslFactory)).option { case hS: HttpsURLConnection ⇒ hS.setHostnameVerifier(hostnameVerifier) case _ ⇒ () }.option { HttpOptions.connTimeout(10000) }.option { HttpOptions.readTimeout(10000) }
 
-  private val ks = KeyStoreTools.getOMInsecureKeyStore
-
-  private val hostnameVerifier: HostnameVerifier = new HostnameVerifier {
-    def verify(p1: String, p2: SSLSession): Boolean = true
-  }
-
-  private var sslFactory = genSSLFactory(ks)
-
   private def genSSLFactory(ks: KeyStore) = {
     val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
     tmf.init(ks)
@@ -85,48 +84,35 @@ case class HTTPControls(address: String, path: String, pass: String) extends OMC
   }
 
   def trustCert() = {
-    val certs = Http(address).option(HttpOptions.allowUnsafeSSL).process(_ match {
-      case h: HttpsURLConnection ⇒ h.getServerCertificates
-      case _                     ⇒ throw new Exception("Can't trust a certificate that doesn't exist.")
-    })
-    val hostName = new URL(address).getHost
-    certs foreach { println }
-    ks.setCertificateEntry(hostName, certs(0))
+    cert foreach { println }
+    cert foreach (ks.setCertificateEntry(hostname, _))
     ks.store(new FileOutputStream(Workspace.file("OMUnsafeKeystore")), "".toCharArray)
     sslFactory = genSSLFactory(ks)
   }
 
-  def isCertTrusted = {
-    val url = new URL(address)
-    val hostName = url.getHost
-    Http(address).option(HttpOptions.allowUnsafeSSL).process(_ match {
-      case h: HttpsURLConnection ⇒ {
-        val certs = h.getServerCertificates
-        ks.getCertificate(hostName) == certs(0)
-      }
-      case _ ⇒ true
-    })
-  }
+  def isCertTrusted = cert map (_ == ks.getCertificate(hostname))
 
-  def getCert = Http(address).option(HttpOptions.allowUnsafeSSL).process(_ match {
+  lazy val hostname = new URL(address).getHost
+
+  def cert = Http(address).option(HttpOptions.allowUnsafeSSL).process(_ match {
     case h: HttpsURLConnection ⇒ {
       val certs = h.getServerCertificates
-      certs(0)
+      Some(certs(0))
     }
-    case _ ⇒ throw new Exception("Can't retrieve a certificate that doesn't exist.")
+    case _ ⇒ None
   })
 }
 
-trait AbstractOMClient extends OMClientInterface {
+abstract class AbstractRESTClient(pass: String) extends RESTClientInterface(pass) {
   def trustCert() = httpControls.trustCert()
   def isCertTrusted = httpControls.isCertTrusted
-  def getCert = httpControls.getCert
+  def cert = httpControls.cert
 
   val httpControls: HTTPControls = new HTTPControls(address, path, pass)
 }
 
-case class WebClient(address: String, pass: String) extends AbstractOMClient {
-  def path = ""
+class WebClient(val address: String, pass: String) extends AbstractRESTClient(pass) {
+  val path = ""
 
   def createMole(moleData: Array[Byte], context: Option[Array[Byte]], pack: Boolean, encapsulate: Boolean): String = {
     httpControls.createMole(moleData, context, pack, encapsulate).asString
@@ -141,8 +127,8 @@ case class WebClient(address: String, pass: String) extends AbstractOMClient {
   def deleteMole(id: String) = httpControls.deleteMole(id).asString
 }
 
-case class XMLClient(address: String, pass: String) extends AbstractOMClient {
-  def path = "xml"
+class XMLClient(val address: String, pass: String) extends AbstractRESTClient(pass) {
+  val path = "xml"
 
   def createMole(moleData: Array[Byte], context: Option[Array[Byte]], pack: Boolean, encapsulate: Boolean) = httpControls.createMole(moleData, context, pack, encapsulate).asXml
 
@@ -155,9 +141,9 @@ case class XMLClient(address: String, pass: String) extends AbstractOMClient {
   def deleteMole(id: String) = httpControls.deleteMole(id).asXml
 }
 
-case class ScalaClient(address: String, pass: String) extends AbstractOMClient {
-  def path = "xml"
-  val subClient = XMLClient(address, pass)
+class ScalaClient(val address: String, pass: String) extends AbstractRESTClient(pass) {
+  val subClient = new XMLClient(address, pass)
+  val path = subClient.path
 
   def createMole(moleData: Array[Byte], context: Option[Array[Byte]], pack: Boolean, encapsulate: Boolean): Either[String, UUID] = {
     val xml = subClient.createMole(moleData, context, pack, encapsulate)
