bae71805f56d81f51d34eb6edf0699553a46ede2
Romain Reuillon <romain.reuillon@iscpif.fr>
2014-03-15 11:37:23 +0100

    Add log for glite job submission.

--- openmole/core/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteJobService.scala
+++ openmole/core/plugins/org.openmole.plugin.environment.glite/src/main/scala/org/openmole/plugin/environment/glite/GliteJobService.scala
@@ -71,6 +71,7 @@ trait GliteJobService extends GridScaleJobService with JobServiceQualityControl 
   override protected def _submit(serializedJob: SerializedJob) = quality {
     delegated {
       import serializedJob._
+      import GliteJobService.Log._
 
       val script = Workspace.newFile("script", ".sh")
       try {
@@ -80,15 +81,15 @@ trait GliteJobService extends GridScaleJobService with JobServiceQualityControl 
 
         val scriptContent = generateScript(serializedJob, outputFilePath, Some(_runningPath), Some(_finishedPath))
 
-        GliteJobService.logger.fine(scriptContent)
-
         Resource.fromFile(script).write(scriptContent)
 
         val jobDescription = buildJobDescription(script)
 
-        //logger.fine(jobDescription.toJDL)
+        logger.fine(s"""Submitting job: "$jobDescription.toJDL" with script "$scriptContent" """)
 
         val jid = jobService.submit(jobDescription)(authentication)
+
+        logger.fine(s"""Job successfully submitted with job service ${jobService.url} and id ${jid}""")
 
         new GliteJob {
           val jobService = js
