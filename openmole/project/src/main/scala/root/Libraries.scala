9801cf3ef06b88bc066ad48cbaca17612f853d09
Romain Reuillon <romain.reuillon@iscpif.fr>
2014-05-24 18:30:35 +0200

    Update gridscale.

--- openmole/project/src/main/scala/root/Libraries.scala
+++ openmole/project/src/main/scala/root/Libraries.scala
@@ -18,7 +18,7 @@ object Libraries extends Defaults(Apache) {
 
   val dir = file("libraries")
 
-  val gridscaleVersion = "1.65"
+  val gridscaleVersion = "1.66"
 
   val bouncyCastleVersion = "1.49"
 
