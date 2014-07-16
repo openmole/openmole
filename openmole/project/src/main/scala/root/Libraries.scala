2b72065f5320135f231d890d9c5070f77263788d
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2014-07-16 18:08:04 +0100

    New GridScale release

--- openmole/project/src/main/scala/root/Libraries.scala
+++ openmole/project/src/main/scala/root/Libraries.scala
@@ -18,7 +18,7 @@ object Libraries extends Defaults(Apache) {
 
   val dir = file("libraries")
 
-  val gridscaleVersion = "1.74"
+  val gridscaleVersion = "1.75-SNAPSHOT"
 
   val bouncyCastleVersion = "1.50"
 
