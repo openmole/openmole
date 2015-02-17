3135fd4ff84672c5ae965155a8903ad5eb2411fb
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-02-17 23:41:43 +0100

    Fix bug, use context with default in it.

--- openmole/core/core/org.openmole.core.workflow/src/main/scala/org/openmole/core/workflow/tools/InputOutputCheck.scala
+++ openmole/core/core/org.openmole.core.workflow/src/main/scala/org/openmole/core/workflow/tools/InputOutputCheck.scala
@@ -91,7 +91,7 @@ trait InputOutputCheck {
     if (!inputErrors.isEmpty) throw new InternalProcessingError(s"Input errors have been found in ${this}: ${inputErrors.mkString(", ")}.")
 
     val result =
-      try context + process(initializedContext)
+      try initializedContext + process(initializedContext)
       catch {
         case e: Throwable ⇒
           throw new InternalProcessingError(e, s"Error for context values in ${this} ${context.prettified()}")
