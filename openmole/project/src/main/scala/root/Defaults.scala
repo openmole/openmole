38bc7b5e6e4520019a6a2b06c0954009cb974e26
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-03-16 23:54:28 +0100

    Update scala version.

--- openmole/project/src/main/scala/root/Defaults.scala
+++ openmole/project/src/main/scala/root/Defaults.scala
@@ -24,7 +24,7 @@ abstract class Defaults(subBuilds: Defaults*) extends BuildSystemDefaults {
   override def settings = super.settings ++
     Seq(
       version in Global := "5.0-SNAPSHOT",
-      scalaVersion in Global := "2.11.5",
+      scalaVersion in Global := "2.11.6",
       scalacOptions ++= Seq("-deprecation"),
       javacOptions in (Compile, compile) ++= Seq("-source", "1.7", "-target", "1.7"),
       scalacOptions += "-target:jvm-1.7",
