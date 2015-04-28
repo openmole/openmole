ea7b215c7e2b2c8613aeb047628a5de806c48450
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-04-28 20:48:12 +0200

    [Build] fix: define repo given the bundle version.

--- build-system/src/main/scala/org/openmole/buildsystem/OsgiBundler.scala
+++ build-system/src/main/scala/org/openmole/buildsystem/OsgiBundler.scala
@@ -23,7 +23,8 @@ trait OsgiBundler {
     OsgiKeys.bundle <<= OsgiKeys.bundle tag Tags.Disk,
     (update in install) <<= update in install tag Tags.Network,
     bundleType := Set("default"),
-    test in (Test, test) <<= test in (Test, test) tag (Tags.Disk)
+    test in (Test, test) <<= test in (Test, test) tag (Tags.Disk),
+    publishTo <<= isSnapshot(if (_) Some("OpenMOLE Nexus" at "http://maven.openmole.org/snapshots") else Some("OpenMOLE Nexus" at "http://maven.openmole.org/releases"))
   ) ++ scalariformDefaults
 
   def OsgiProject(artifactSuffix: String,
