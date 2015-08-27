32ebc1304c7e5e5118406976e59b72d138e2a07b
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2015-08-27 17:45:27 +0200

    [GUI] Implements Doc architecture

--- openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/GUIDoc.scala
+++ openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/GUIDoc.scala
@@ -4,9 +4,12 @@ import org.scalajs.dom.raw.HTMLDivElement
 
 import scalatags.JsDom.TypedTag
 import scalatags.JsDom.{ tags ⇒ tags }
-import org.openmole.gui.misc.js.{ BootstrapTags ⇒ bs }
+import org.openmole.gui.misc.js.JsRxTags._
+import org.openmole.gui.misc.js.{ BootstrapTags ⇒ bs, ClassKeyAggregator }
 import scalatags.JsDom.all._
 import bs._
+import rx._
+
 /*
  * Copyright (C) 27/08/15 // mathieu.leclaire@openmole.org
  *
@@ -26,8 +29,53 @@ import bs._
 
 object GUIDoc {
 
+  case class GUIDocEntry(glyph: ClassKeyAggregator, title: String, content: TypedTag[HTMLDivElement])
+
+  val selectedEntry: Var[Option[GUIDocEntry]] = Var(None)
+
+  val entries = Seq(
+    GUIDocEntry(bs.glyph_file, "Manage the resources", tags.div("manage file")),
+    GUIDocEntry(bs.glyph_settings, "Execute scripts", tags.div("Execute")),
+    GUIDocEntry(bs.glyph_lock, "Manage authentications", tags.div("authentications")),
+    GUIDocEntry(bs.glyph_market, "The Market place", tags.div("market place")),
+    GUIDocEntry(bs.glyph_plug, "Plugins", tags.div("plugins"))
+  )
+
   val doc: TypedTag[HTMLDivElement] = {
-    tags.div("Doc !")
+    tags.div(`class` := "docText",
+      "This help provides informations to use the OpenMOLE web application. ",
+      tags.div(
+        "This application enables to manage OpenMOLE scripts, to edit them and to run them." +
+          " It does not explain how to build a workflow by means of the ",
+        tags.a(href := "http://www.openmole.org/current/Documentation_Language.html", target := "_blank")(
+          "OpenMOLE language"),
+        ", which is explained in detail on the OpenMOLE website."),
+      for (entry ← entries) yield {
+        Rx {
+          val isSelected = selectedEntry() == Some(entry)
+          tags.div(
+            `class` := "docEntry" + { if (isSelected) " docEntrySelected" else "" },
+            tags.span(
+              `class` := "docTitleEntry",
+              onclick := { () ⇒
+                {
+                  selectedEntry() = {
+                    if (isSelected) None
+                    else Some(entry)
+                  }
+                }
+              },
+              glyph(entry.glyph + "glyphText"),
+              entry.title
+            ),
+            if (isSelected) tags.div(`class` := "docContent", entry.content)
+            else tags.div
+
+          )
+        }
+      }
+    )
+
   }
 
 }
