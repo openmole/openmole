1de4e2cc6881a6fd5ca0e637b45dec495a23e36d
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2015-08-31 15:25:29 +0200

    [GUI] Cleanup

--- openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/PluginPanel.scala
+++ openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/PluginPanel.scala
@@ -41,7 +41,6 @@ class PluginPanel extends ModalPanel {
   }
 
   def onClose() = {
-    println("clossssssse")
   }
 
   def getPlugins = {
@@ -56,7 +55,6 @@ class PluginPanel extends ModalPanel {
       FileManager.upload(fileInput,
         SafePath.empty,
         (p: FileTransferState) ⇒ {
-          println("Transfer: " + p.ratio + " " + p.display)
           transferring() = p
         },
         UploadPlugin(),
