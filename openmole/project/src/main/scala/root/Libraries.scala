a5fee5ad170b880057519deb694df0ee5c055bb8
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-09-17 15:21:00 +0200

    [Build] enh: update scala version.

--- openmole/project/src/main/scala/root/Libraries.scala
+++ openmole/project/src/main/scala/root/Libraries.scala
@@ -67,7 +67,7 @@ object Libraries extends Defaults {
 
   lazy val groovy = "org.openmole" %% "org-codehaus-groovy" % "2.4.1"
 
-  lazy val scalaLang = "org.openmole" %% "org-scala-lang-scala-library" % "2.11.6"
+  lazy val scalaLang = "org.openmole" %% "org-scala-lang-scala-library" % "2.11.7"
 
   lazy val jodaTime = "org.openmole" %% "org-joda-time" % "1.6"
 
