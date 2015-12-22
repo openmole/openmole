8894764bb44afa6e4ff8c8079294d19f26f96273
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2015-12-22 19:34:10 +0100

    [GUI] Set waiters for Market place panel

---
+++ openmole/gui/client/org.openmole.gui.client.core/src/main/scala/CoreUtils.scala
@@ -0,0 +1,31 @@
+package org.openmole.gui.client.core
+/*
+ * Copyright (C) 22/12/15 // mathieu.leclaire@openmole.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+object CoreUtils {
+
+  implicit class SeqUpdater[T](sequence: Seq[T]) {
+    def updatedFirst(t: T, s: T): Seq[T] = {
+      val index = sequence.indexOf(t)
+      if (index != -1) sequence.updated(index, s)
+      else sequence
+    }
+
+    def updatedFirst(cond: T ⇒ Boolean, s: T): Seq[T] =
+      sequence.find(cond).map { e ⇒ updatedFirst(e, s) }.getOrElse(sequence)
+  }
+}
