b85a88a35f56a2c8ee206a24de5d8c49749840f4
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-12-29 13:59:10 +0100

    [Plugin] enh: enable webdav service usage for egi.

--- openmole/plugins/org.openmole.plugin.environment.egi/src/main/scala/org/openmole/plugin/environment/egi/BDIIStorageServers.scala
+++ openmole/plugins/org.openmole.plugin.environment.egi/src/main/scala/org/openmole/plugin/environment/egi/BDIIStorageServers.scala
@@ -49,16 +49,16 @@ trait BDIIStorageServers extends BatchEnvironment { env ⇒
 
   @transient lazy val storages = {
     def timeout = Workspace.preferenceAsDuration(EGIEnvironment.FetchResourcesTimeOut)
-    /*val webdavStorages = bdiiServer.queryWebDAVLocations(voName, timeout)
+    val webdavStorages = bdiiServer.queryWebDAVLocations(voName, timeout)
     if (!webdavStorages.isEmpty) {
       logger.fine("Use webdav storages:" + webdavStorages.mkString(","))
       webdavStorages.map { s ⇒ EGIWebDAVStorageService(s, env, proxyCreator) }
     }
-    else {*/
-    val srmStorages = bdiiServer.querySRMLocations(voName, timeout)
-    logger.fine("Use srm storages:" + srmStorages.mkString(","))
-    srmStorages.map { s ⇒ EGISRMStorageService(s, env, proxyCreator) }
-    //}
+    else {
+      val srmStorages = bdiiServer.querySRMLocations(voName, timeout)
+      logger.fine("Use srm storages:" + srmStorages.mkString(","))
+      srmStorages.map { s ⇒ EGISRMStorageService(s, env, proxyCreator) }
+    }
   }
 
   def trySelectAStorage(usedFileHashes: Iterable[(File, Hash)]) = {
