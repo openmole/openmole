ce799bfcd4d78c67828f910bcd7d42224cbdb616
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2016-01-06 21:27:10 +0000

    fix: Remove CARE dependency from GUI core (led to bundle installed twice)

--- openmole/project/src/main/scala/root/gui/GuiServer.scala
+++ openmole/project/src/main/scala/root/gui/GuiServer.scala
@@ -8,7 +8,6 @@ import root._
 import root.Libraries._
 import sbt.Keys._
 import com.typesafe.sbt.osgi.OsgiKeys._
-import _root_.plugin.Task
 import ThirdParties._
 
 object Server extends GuiDefaults {
@@ -28,8 +27,7 @@ object Server extends GuiDefaults {
       root.Runtime.console,
       Misc.utils,
       root.ThirdParties.openmoleStream,
-      openmoleCrypto,
-      Task.care
+      openmoleCrypto
     )
 
   lazy val state = OsgiProject("org.openmole.gui.server.state") settings
