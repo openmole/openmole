ad1d07b793eac3e029d78513fb205843bb3d5b9e
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-08-31 10:42:27 +0200

    [Build] fix: update scalatex

--- openmole/project/src/main/scala/root/Libraries.scala
+++ openmole/project/src/main/scala/root/Libraries.scala
@@ -138,7 +138,7 @@ object Libraries extends Defaults {
 
   lazy val scalatest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
-  lazy val scalatexSite = "org.openmole" %% "com-lihaoyi-scalatex-site" % "0.3.5"
+  lazy val scalatexSite = "org.openmole" %% "com-lihaoyi-scalatex-site" % "0.3.6"
 
   lazy val math = "org.openmole" %% "org-apache-commons-math" % "3.5"
 
