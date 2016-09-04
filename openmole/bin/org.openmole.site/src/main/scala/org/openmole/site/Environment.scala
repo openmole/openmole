f2b59d197b539fbc59b1d3e6d1e2d575c51c2d4e
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2016-09-04 14:59:54 +0100

    [Doc] add: name entry for enviroments + refactor prettyfier

--- openmole/bin/org.openmole.site/src/main/scala/org/openmole/site/Environment.scala
+++ openmole/bin/org.openmole.site/src/main/scala/org/openmole/site/Environment.scala
@@ -20,15 +20,17 @@ import scalatags.Text.all._
 
 object Environment {
 
-  def envOption(option: String): Frag = Seq[Frag](b(option), ": ")
+  def envEntryTitle(entryName: String): Frag = Seq[Frag](b(entryName), ": ")
+  def newEntry(name: String, body: Frag*): Frag = Seq[Frag](envEntryTitle(name), body)
 
   def provideOptions = """You also can set options by providing additional parameters to the environment (..., option = value, ...)"""
-  def wallTime: Frag = Seq[Frag](envOption("walltime"), " the maximum duration for the job in term of user time, for instance ", hl.openmoleNoTest("wallTime = 1 hour"))
-  def memory: Frag = Seq[Frag](envOption("memory"), " the memory in mega-byte for the job, for instance ", hl.openmoleNoTest("memory = 2000"))
-  def openMOLEMemory: Frag = Seq[Frag](envOption("openMOLEMemory"), " the memory of attributed to the OpenMOLE runtime on the execution node, if you run external tasks you can reduce the memory for the OpenMOLE runtime to 256MB in order to have more memory for you program on the execution node, for instance ", hl.openmoleNoTest("openMOLEMemory = 256"))
-  def threads: Frag = Seq[Frag](envOption("threads"), " the number of threads for concurrent execution of tasks on the worker node, for instance ", hl.openmoleNoTest("threads = 4"))
-  def queue: Frag = Seq[Frag](envOption("queue"), " the name of the queue on which jobs should be submitted, for instance ", hl.openmoleNoTest("queue = \"longjobs\""))
-  def port: Frag = Seq[Frag](envOption("port"), " the number of the port used by the ssh server, by ", b("default it is set to 22"))
-  def sharedDirectory: Frag = Seq[Frag](envOption("sharedDirectory"), " OpenMOLE uses this directory to communicate from the head of the cluster to the worker nodes (", hl.openmoleNoTest("sharedDirectory = \"/home/user/openmole\""))
-  def workDirectory: Frag = Seq[Frag](envOption("workDirectory"), " the directory in which OpenMOLE will run on the remote server, for instance ", hl.openmoleNoTest("workDirectory = \"${TMP}\""))
+  def wallTime = newEntry("walltime", " the maximum duration for the job in term of user time, for instance ", hl.openmoleNoTest("wallTime = 1 hour"))
+  def memory = newEntry("memory", " the memory in mega-byte for the job, for instance ", hl.openmoleNoTest("memory = 2000"))
+  def openMOLEMemory = newEntry("openMOLEMemory", " the memory of attributed to the OpenMOLE runtime on the execution node, if you run external tasks you can reduce the memory for the OpenMOLE runtime to 256MB in order to have more memory for you program on the execution node, for instance ", hl.openmoleNoTest("openMOLEMemory = 256"))
+  def threads = newEntry("threads", " the number of threads for concurrent execution of tasks on the worker node, for instance ", hl.openmoleNoTest("threads = 4"))
+  def queue = newEntry("queue", " the name of the queue on which jobs should be submitted, for instance ", hl.openmoleNoTest("queue = \"longjobs\""))
+  def port = newEntry("port", " the number of the port used by the ssh server, by ", b("default it is set to 22"))
+  def sharedDirectory = newEntry("sharedDirectory", " OpenMOLE uses this directory to communicate from the head of the cluster to the worker nodes (", hl.openmoleNoTest("sharedDirectory = \"/home/user/openmole\""))
+  def workDirectory = newEntry("workDirectory", " the directory in which OpenMOLE will run on the remote server, for instance ", hl.openmoleNoTest("workDirectory = \"${TMP}\""))
+  def name = newEntry("name", "the name an environment will take in the logs")
 }
