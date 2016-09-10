26798743625b771c6e6eb3d2e7d6a544c93ce21b
Romain Reuillon <romain.reuillon@iscpif.fr>
2016-09-10 16:19:04 +0200

    Revert "[Build] enh: embed the jvm inside the batch plugin."
    This reverts commit 1a8d73ede6fbc092243cf7059f280a67c2fba7f4.

--- openmole/plugins/org.openmole.plugin.environment.batch/src/main/scala/org/openmole/plugin/environment/batch/environment/BatchEnvironment.scala
+++ openmole/plugins/org.openmole.plugin.environment.batch/src/main/scala/org/openmole/plugin/environment/batch/environment/BatchEnvironment.scala
@@ -43,7 +43,6 @@ import org.openmole.core.communication.message._
 import org.openmole.core.console.ScalaREPL.ReferencedClasses
 import org.openmole.core.console.{ REPLClassloader, ScalaREPL }
 import org.openmole.tool.cache._
-import org.openmole.tool.stream._
 
 import ref.WeakReference
 import scala.Predef.Set
@@ -125,6 +124,7 @@ object BatchEnvironment extends Logger {
   private def runtimeDirLocation = Workspace.openMOLELocation / "runtime"
 
   lazy val runtimeLocation = runtimeDirLocation / "runtime.tar.gz"
+  lazy val JVMLinuxX64Location = runtimeDirLocation / "jvm-x64.tar.gz"
 
   Workspace setDefault MemorySizeForRuntime
   Workspace setDefault CheckInterval
@@ -139,15 +139,6 @@ object BatchEnvironment extends Logger {
   def getTokenInterval = Workspace.preference(GetTokenInterval) * Workspace.rng.nextDouble
 
   lazy val jobManager = new JobManager
-
-  def jvmLinuxX64Location = "org/openmole/plugin/environment/batch/jvm-x64.tar.gz"
-
-  lazy val jvmLinuxX64 = {
-    val cp = Workspace.newFile("jvm-x64", ".tar.gz")
-    withClosable { this.getClass.getClassLoader.getResourceAsStream(jvmLinuxX64Location) } { is ⇒ is copy cp }
-    cp
-  }
-
 }
 
 import BatchEnvironment._
@@ -200,7 +191,7 @@ trait BatchExecutionJob extends ExecutionJob { bej ⇒
 
   def usedFiles: Iterable[File] =
     (files ++
-      Seq(environment.runtime, BatchEnvironment.jvmLinuxX64) ++
+      Seq(environment.runtime, environment.jvmLinuxX64) ++
       environment.plugins() ++ plugins).distinct
 
   def usedFileHashes = usedFiles.map(f ⇒ (f, FileService.hash(job.moleExecution, f)))
@@ -225,7 +216,7 @@ trait BatchEnvironment extends SubmissionEnvironment { env ⇒
     case Some(m) ⇒ m
   }
 
-  @transient lazy val batchJobWatcher = Cache {
+  @transient val batchJobWatcher = Cache {
     val watcher = new BatchJobWatcher(WeakReference(this))
     Updater.registerForUpdate(watcher)
     watcher
@@ -242,7 +233,7 @@ trait BatchEnvironment extends SubmissionEnvironment { env ⇒
   }
 
   def runtime = BatchEnvironment.runtimeLocation
-  def jvmLinuxX64 = BatchEnvironment.jvmLinuxX64
+  def jvmLinuxX64 = BatchEnvironment.JVMLinuxX64Location
 
   @transient val plugins = Cache { PluginManager.pluginsForClass(this.getClass) }
 
