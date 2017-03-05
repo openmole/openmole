3e88ba5b463f34d6c090fcc9531e27fef0c54d42
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-03-05 22:06:31 +0700

    [Daemon] fix: add missing file.

---
+++ openmole/bin/org.openmole.daemon/src/main/scala/org/openmole/daemon/Activator.scala
@@ -0,0 +1,13 @@
+package org.openmole.daemon
+
+import org.openmole.core.workspace.ConfigurationInfo
+import org.osgi.framework.{ BundleActivator, BundleContext }
+
+class Activator extends BundleActivator {
+  override def stop(context: BundleContext): Unit = {
+    ConfigurationInfo.remove(JobLauncher.getClass)
+  }
+  override def start(context: BundleContext): Unit = {
+    ConfigurationInfo.add(JobLauncher.getClass, ConfigurationInfo.list(JobLauncher))
+  }
+}
