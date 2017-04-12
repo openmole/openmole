3c42e639c6ee7a95040815ead4ad1c3fc48a3b70
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-04-12 11:26:46 +0200

    [App] enh: adjust memory on windows if it is 32 bits.

--- openmole/bin/openmole-naked/src/main/resources/openmole.bat
+++ openmole/bin/openmole-naked/src/main/resources/openmole.bat
@@ -6,15 +6,19 @@ start /MIN dbserver\bin\openmole-dbserver.bat
 cd %cd%
 mkdir "%UserProfile%\.openmole\.tmp"
 set ran="%UserProfile%\.openmole\.tmp\%random%"
+
+set MEMORY=1500
+
 java -d64 -version >nul 2>&1
 if errorlevel 1 goto is32bit
+set MEMORY=2000
 set FLAG="-XX:+UseCompressedOops"
 :is32bit
 
 set SCRIPT_ARGS=
 
 :run
-java -Xss2M -Dfile.encoding=UTF-8 -Dosgi.locking=none -Dopenmole.location="%PWD%" -Dosgi.classloader.singleThreadLoads=true -Dosgi.configuration.area=%ran% -XX:MaxPermSize=128M -XX:+UseG1GC -XX:CICompilerCount=2 -XX:ParallelGCThreads=2 -XX:ConcGCThreads=2 -XX:G1ConcRefinementThreads=2 -Xmx1500m -XX:MaxPermSize=128M %FLAG% -cp "%PWD%/launcher/*" org.openmole.launcher.Launcher  --plugins "%PWD%/plugins/" --run org.openmole.ui.Application --priority "logging" --osgi-directory %ran% -- %* %SCRIPT_ARGS%
+java -Xss2M -Dfile.encoding=UTF-8 -Dosgi.locking=none -Dopenmole.location="%PWD%" -Dosgi.classloader.singleThreadLoads=true -Dosgi.configuration.area=%ran% -XX:MaxPermSize=128M -XX:+UseG1GC -XX:CICompilerCount=2 -XX:ParallelGCThreads=2 -XX:ConcGCThreads=2 -XX:G1ConcRefinementThreads=2 -Xmx%MEMORY%m -XX:MaxPermSize=128M %FLAG% -cp "%PWD%/launcher/*" org.openmole.launcher.Launcher  --plugins "%PWD%/plugins/" --run org.openmole.ui.Application --priority "logging" --osgi-directory %ran% -- %* %SCRIPT_ARGS%
 set ret=%errorlevel%
 rmdir /s /q %ran%
 
