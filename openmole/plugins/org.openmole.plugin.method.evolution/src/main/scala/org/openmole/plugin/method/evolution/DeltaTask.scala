9f7840e793754d7908a3132f138de58be19d0d52
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-06-23 14:05:17 +0200

    [Plugis] add: implement delta pattern.

---
+++ openmole/plugins/org.openmole.plugin.method.evolution/src/main/scala/org/openmole/plugin/method/evolution/DeltaTask.scala
@@ -0,0 +1,22 @@
+package org.openmole.plugin.method.evolution
+
+import org.openmole.core.dsl._
+import org.openmole.core.workflow.task.ClosureTask
+import org.openmole.core.context.Variable
+import org.openmole.core.workflow.puzzle.Puzzle
+
+object DeltaTask {
+  def apply(objective: (Val[Double], Double)*)(implicit name: sourcecode.Name) =
+    ClosureTask("DeltaTask") { (context, _, _) ⇒
+      context ++ objective.map { case (v, o) ⇒ Variable(v, math.abs(context(v) - o)) }
+    } set (
+      (inputs, outputs) += (objective.map(_._1): _*)
+    )
+}
+
+object Delta {
+
+  def apply(puzzle: Puzzle, objective: (Val[Double], Double)*) =
+    puzzle -- DeltaTask(objective: _*)
+
+}
