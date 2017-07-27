28dd33013f9fffddfa0990875fd5605a64b217ed
Mathieu <mathieu.leclaire@iscpif.fr>
2017-07-27 23:41:03 +0200

    [GUI] Fix compilation

--- openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/ID.scala
+++ openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/ID.scala
@@ -19,8 +19,11 @@ package org.openmole.gui.client.tool
 
 import java.util.UUID
 
-import scala.scalajs.js.annotation.JSExport
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 object ID {
   def get: String = UUID.randomUUID.toString
 }
+
+@JSExportTopLevel("ID")
+case class ID(id: String = ID.get)
