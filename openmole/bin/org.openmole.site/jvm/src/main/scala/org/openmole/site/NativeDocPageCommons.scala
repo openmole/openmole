99e7bc516c7fa82548393addb20ca4286a7bff5f
Mathieu <mathieu.leclaire@iscpif.fr>
2017-08-10 15:03:03 +0200

    [Site] Fix typos

--- openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/NativeDocPageCommons.scala
+++ openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/NativeDocPageCommons.scala
@@ -40,7 +40,7 @@ object NativeDocPageCommons {
 
   def introPackagingForRPythonCplusplus = paragraph(
     "Most of the time, model code is not designed to be portable. For now, OpenMOLE handles Java, Scala, NetLogo and R (in the near future) via specific Tasks, but it is still far from covering the languages used to develop models around the world.",
-    p("Meanwhile, you have to package your code using CARE, as explained on ", a("this page", href := DocumentationPages.nativePackaging.file), ". The followings contents expose directly how to handle your packaged model within OpenMOLE")
+    p("Meanwhile, you have to package your code using CARE, as explained in the ", a("Native Packaging section", href := DocumentationPages.nativePackaging.file, targetBlank), ". The followings contents expose directly how to handle your packaged model within OpenMOLE")
   )
 
 }
