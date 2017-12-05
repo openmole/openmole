bb739a3f6dd4679806584e4c2496fb3b7291de2e
Romain Reuillon <romain.reuillon@iscpif.fr>
2017-12-05 07:20:24 +0700

    [Lib] update bouncy castle.

--- openmole/project/Libraries.scala
+++ openmole/project/Libraries.scala
@@ -5,7 +5,7 @@ import sbt._
 object Libraries {
 
   lazy val gridscaleVersion = "2.0"
-  lazy val bouncyCastleVersion = "1.50"
+  lazy val bouncyCastleVersion = "1.57"
   lazy val aceVersion = "01.08.2014"
   lazy val d3Version = "3.5.12"
   lazy val tooltipserVersion = "3.3.0"
