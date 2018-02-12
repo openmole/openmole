9148bc4b07cabf8f57d2e5ebdfa2e871ed565d65
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-02-12 15:05:02 +0100

    [Core] enh: remove useless imports.

--- openmole/core/org.openmole.core.code/src/test/scala/org/openmole/core/code/RNGSpec.scala
+++ openmole/core/org.openmole.core.code/src/test/scala/org/openmole/core/code/RNGSpec.scala
@@ -1,6 +1,5 @@
 package org.openmole.core.code
 
-import org.apache.commons.math3.random.{ SynchronizedRandomGenerator, Well44497b }
 import org.scalatest._
 
 class RNGSpec extends FlatSpec with Matchers {
