8e42fb23f4e338b6df525b65d68566e0af2b9cb4
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-03-23 13:54:39 +0100

    [lib] enh: update netlogo

--- openmole/project/Libraries.scala
+++ openmole/project/Libraries.scala
@@ -10,7 +10,7 @@ object Libraries {
   lazy val d3Version = "3.5.12"
   lazy val tooltipserVersion = "3.3.0"
   lazy val netLogo5Version = "5.3.1"
-  lazy val netLogo6Version = "6.0.2"
+  lazy val netLogo6Version = "6.0.3"
   lazy val scalaTagsVersion = "0.6.5"
   lazy val scalaJSDomVersion = "0.9.3"
   lazy val rxVersion = "0.3.2"
