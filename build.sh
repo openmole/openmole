2f7fec546d6ed8cf7e31de16d2c7509914216d2d
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-03-29 02:13:49 +0200

    Add build and clean script.

---
+++ build.sh
@@ -0,0 +1,10 @@
+git submodule init
+git submodule update
+git lfs fetch
+
+(cd build-system && sbt publishLocal)
+(cd libraries && sbt publishLocal)
+(cd openmole && sbt "project openmole" assemble)
+
+(echo openmole is ready in openmole/bin/openmole/assemble/)
+
---
+++ clean.sh
@@ -0,0 +1,1 @@
+git clean -d -x -f
