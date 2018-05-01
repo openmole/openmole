dc4ad49cd31e410f091ed9f65cc62e5d9bec33ca
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-05-01 17:40:49 +0200

    [Plugin] fix: update mgo.

--- openmole/project/Libraries.scala
+++ openmole/project/Libraries.scala
@@ -5,6 +5,7 @@ import sbt._
 object Libraries {
 
   lazy val gridscaleVersion = "2.4"
+  lazy val mgoVersion = "3.12"
   lazy val bouncyCastleVersion = "1.57"
   lazy val aceVersion = "01.08.2014"
   lazy val d3Version = "3.5.12"
@@ -79,7 +80,7 @@ object Libraries {
   lazy val circe = "org.openmole.library" %% "io-circe" % circeVersion
   lazy val scalajsTools = "org.openmole.library" %% "scalajs-tools" % scalajsVersion
   lazy val scalajs = "org.openmole.library" %% "scalajs" % scalajsVersion
-  lazy val mgo = "org.openmole.library" %% "mgo" % "3.11"
+  lazy val mgo = "org.openmole.library" %% "mgo" % mgoVersion
   lazy val family = "org.openmole.library" %% "fr-iscpif-family" % "1.3"
   lazy val monocle = "org.openmole.library" %% "monocle" % "1.5.0"
   lazy val boopickle = "org.openmole.library" %% "boopickle" % boopickleVersion
