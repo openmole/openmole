8158f6a78d88872a98f3de9f3795039a6603ad01
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-06-12 14:52:11 +0200

    [Plugin] enh: implement detail for failed job execution.

---
+++ openmole/plugins/org.openmole.plugin.environment.batch/src/main/scala/org/openmole/plugin/environment/batch/environment/package.scala
@@ -0,0 +1,7 @@
+package org.openmole.plugin.environment.batch
+
+import org.openmole.core.exception.InternalProcessingError
+
+package object environment {
+  case class FailedJobExecution(message: String, cause: Throwable, detail: String) extends InternalProcessingError(message, cause)
+}
