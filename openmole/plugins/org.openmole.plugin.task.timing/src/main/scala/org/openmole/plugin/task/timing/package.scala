5a6cfae58598e4e60ebb71eddc1cb74ccf12f687
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-06-29 15:08:06 +0200

    [Plugin] TimingTask : fix dsl

--- openmole/plugins/org.openmole.plugin.task.timing/src/main/scala/org/openmole/plugin/task/timing/package.scala
+++ openmole/plugins/org.openmole.plugin.task.timing/src/main/scala/org/openmole/plugin/task/timing/package.scala
@@ -3,12 +3,15 @@ package org.openmole.plugin.task
 package timing {
 
   import org.openmole.core.context._
+  import org.openmole.core.workflow.builder.{ DefinitionScope, InputOutputBuilder }
   import org.openmole.core.workflow.task._
 
   trait TimingTaskPackage {
 
-    implicit class TimingTaskDecorator(task: Task){
-      def withTimer(tracker: Val[Long]): Task = TimingTask(task,tracker)
+    implicit def builder: InputOutputBuilder[Task] = InputOutputBuilder(TimingTask.config).asInstanceOf[InputOutputBuilder[Task]]
+
+    implicit class TimingTaskDecorator(task: Task) {
+      def withTimer(tracker: Val[Long])(implicit name: sourcecode.Name, definitionScope: DefinitionScope): Task = TimingTask(task, tracker)
     }
 
   }
