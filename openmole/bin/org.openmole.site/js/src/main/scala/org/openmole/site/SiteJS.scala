cde7443d52664905b8a25fa3061dfbd4b91be1c2
Mathieu <mathieu.leclaire@iscpif.fr>
2018-08-08 23:44:51 +0200

    [Site] Generate contents at compile time

--- openmole/bin/org.openmole.site/js/src/main/scala/org/openmole/site/SiteJS.scala
+++ openmole/bin/org.openmole.site/js/src/main/scala/org/openmole/site/SiteJS.scala
@@ -91,23 +91,4 @@ object SiteJS extends JSApp {
 
   @JSExport
   def sensitivityAnimation(): Unit = SVGStarter.decorateTrigger(shared.sensitivity.button, shared.sensitivity.animation, 8000)
-
-  @JSExport
-  def documentationSideMenu(): Unit = {
-    val nodes = org.scalajs.dom.document.getElementsByClassName(shared.documentationSideMenu.cssClass)
-
-    val text = p((0 until nodes.length).map(i ⇒ nodes(i).textContent).mkString(", "))
-
-    val menuContent =
-      for {
-        i ← 0 until nodes.length
-      } yield {
-        val text = nodes(i).textContent.dropRight(2)
-        nodes(i).nodeName match {
-          case "H2" ⇒ Some(div(paddingTop := 5)(a(href := "#" + shared.anchor(text))(text)))
-          case _    ⇒ None
-        }
-      }
-    org.scalajs.dom.window.document.getElementById(shared.documentationSideMenu.place).innerHTML = div(if (nodes.length < 1) span(marginTop := 40) else h2(marginTop := -20, "Contents"))(menuContent.flatten: _*)
-  }
 }
