7a5f5b2252b49a5cb8a68d31b36c384af0a69bc2
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-09-18 17:26:27 +0200

    [Plugin] enh: use puzzle container in Direct Sampling

--- openmole/plugins/org.openmole.plugin.method.directsampling/src/main/scala/org/openmole/plugin/method/directsampling/package.scala
+++ openmole/plugins/org.openmole.plugin.method.directsampling/src/main/scala/org/openmole/plugin/method/directsampling/package.scala
@@ -18,19 +18,17 @@
 package org.openmole.plugin.method
 
 import org.openmole.core.context._
-import org.openmole.core.outputmanager.OutputManager
+import org.openmole.core.expansion._
 import org.openmole.core.workflow.builder.DefinitionScope
 import org.openmole.core.workflow.dsl._
 import org.openmole.core.workflow.mole._
 import org.openmole.core.workflow.puzzle._
 import org.openmole.core.workflow.sampling._
 import org.openmole.core.workflow.task._
-import org.openmole.core.workflow.validation.DataflowProblem._
-import org.openmole.core.workflow.validation._
+import org.openmole.core.workflow.transition._
 import org.openmole.plugin.domain.distribution._
 import org.openmole.plugin.domain.modifier._
 import org.openmole.plugin.tool.pattern._
-import org.openmole.core.expansion._
 
 package object directsampling {
 
@@ -57,13 +55,25 @@ package object directsampling {
   ): Puzzle = {
     val exploration = ExplorationTask(sampling)
     val explorationCapsule = Capsule(exploration, strain = true)
+    val wrapped = wrapPuzzle(evaluation, sampling.prototypes.toSeq, evaluation.outputs)
 
     aggregation.option match {
       case Some(aggregation) ⇒
-        (explorationCapsule -< (evaluation when condition) >- aggregation) &
+        val p = (explorationCapsule -< (wrapped.evaluationPuzzle when condition) >- aggregation) &
           (explorationCapsule -- (aggregation block (evaluation.outputs: _*)))
+
+        OutputEnvironmentPuzzleContainer(p, wrapped.evaluationPuzzle.last, wrapped.delegate)
       case None ⇒
-        explorationCapsule -< (evaluation when condition)
+        val preTask = EmptyTask() set ((inputs, outputs) ++= sampling.prototypes)
+        val afterTask = EmptyTask() set ((inputs, outputs) ++= evaluation.outputs ++ sampling.prototypes)
+
+        val preCapsule = Capsule(preTask)
+        val afterSlot = Slot(afterTask)
+
+        val p = (explorationCapsule -< preCapsule -- (evaluation when condition) -- afterSlot) &
+          (preCapsule -- (afterSlot block (evaluation.outputs: _*)))
+
+        OutputEnvironmentPuzzleContainer(p, wrapped.evaluationPuzzle.last, wrapped.delegate)
     }
   }
 
