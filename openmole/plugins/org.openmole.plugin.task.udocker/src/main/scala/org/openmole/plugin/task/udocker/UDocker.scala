71857b9563e63f3bcf22d35278e132243ff14218
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-10-25 14:22:21 +0200

    [Plugin][UDockerTask] Add lock when generating image manifest

--- openmole/plugins/org.openmole.plugin.task.udocker/src/main/scala/org/openmole/plugin/task/udocker/UDocker.scala
+++ openmole/plugins/org.openmole.plugin.task.udocker/src/main/scala/org/openmole/plugin/task/udocker/UDocker.scala
@@ -121,9 +121,12 @@ object UDocker {
 
     val imageId = s"${localDockerImage.image}:${localDockerImage.tag}"
 
-    imageRepositoryDirectory / "manifest" content = localDockerImage.manifest.asJson.toString
-    imageRepositoryDirectory / "TAG" content = s"$repositoryDirectory/$imageId"
-    imageRepositoryDirectory / "v2" content = ""
+    // also lock as this directory can be simultaneously used by other tasks working with the same image
+    imageRepositoryDirectory.withLockInDirectory {
+      imageRepositoryDirectory / "manifest" content = localDockerImage.manifest.asJson.toString
+      imageRepositoryDirectory / "TAG" content = s"$repositoryDirectory/$imageId"
+      imageRepositoryDirectory / "v2" content = ""
+    }
   }
 
   /**
@@ -174,7 +177,7 @@ object UDocker {
     else dockerImage.file.extractUncompress(extractedImage)
 
     val manifestContent = (extractedImage / "manifest.json").content
-    assert(manifestContent.size > 2,s"Corrupted image with no manifest : $extractedImage for file ${dockerImage.file}")
+    assert(manifestContent.size > 0, s"Corrupted saved image with no manifest for file ${dockerImage.file}")
     val topLevelManifests = decode[List[TopLevelImageManifest]](manifestContent)
     val topLevelImageManifest = topLevelManifests.map(_.head)
 
@@ -315,7 +318,6 @@ object UDocker {
       uDocker.localDockerImage.container match {
         case None ⇒
           val cl = commandLine(s"/usr/bin/env python2 ${uDockerExecutable.getAbsolutePath} create $imageId")
-          // check that json conf actually exists ? at creation check that effectively created
           val execres = execute(cl, tmpDirectory, uDockerVariables, captureOutput = true, captureError = true, displayOutput = false, displayError = false).output.get
           (execres.split("\n").map(_.trim).reverse.find(_.size > 0).head, execres)
         case Some(directory) ⇒
