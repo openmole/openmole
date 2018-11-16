77f54f8eb1b599f8febc89685b1994fa61be79f4
Romain Reuillon <romain.reuillon@iscpif.fr>
2018-11-16 15:02:06 +0100

    [Lib] enh: update gridscale

--- openmole/project/Libraries.scala
+++ openmole/project/Libraries.scala
@@ -5,7 +5,7 @@ import sbt._
 
 object Libraries {
 
-  lazy val gridscaleVersion = "2.13"
+  lazy val gridscaleVersion = "2.14"
   lazy val mgoVersion = "3.17"
   lazy val bouncyCastleVersion = "1.60"
   lazy val aceVersion = "01.08.2014"
