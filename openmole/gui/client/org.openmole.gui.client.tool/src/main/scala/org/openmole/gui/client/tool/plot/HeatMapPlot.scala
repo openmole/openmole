2066ef7b5382d47a8dd4a98756317190dfb61145
Mathieu <mathieu.leclaire@iscpif.fr>
2019-03-05 21:38:27 +0100

    [GUI] Add HeatMap plot

---
+++ openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/plot/HeatMapPlot.scala
@@ -0,0 +1,39 @@
+package org.openmole.gui.client.tool.plot
+
+import com.definitelyscala.plotlyjs._
+import com.definitelyscala.plotlyjs.all._
+import com.definitelyscala.plotlyjs.PlotlyImplicits._
+import scala.scalajs.js.JSConverters._
+import scalatags.JsDom.all._
+
+object HeatMapPlot {
+
+  def apply(
+    title:  String  = "",
+    serie:  Serie,
+    legend: Boolean = false) = {
+
+    lazy val plotDiv = Plot.baseDiv
+
+    val dims = serie.values
+    val nbDims = dims.length
+
+    val data = PlotData
+      .z(dims.map { _.values.toJSArray }.toJSArray)
+      .set(plotlytype.heatmap)
+
+    val layout = Layout
+      .title(title)
+      .height(700)
+      .width(700)
+
+    Plotly.newPlot(
+      plotDiv,
+      scalajs.js.Array(data),
+      layout,
+      Plot.baseConfig
+    )
+
+    div(plotDiv.render).render
+  }
+}
