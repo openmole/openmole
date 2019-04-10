aacd09a4eb28a56afc0d332c7de78cd92be87dad
Romain Reuillon <romain.reuillon@iscpif.fr>
2019-04-10 18:34:03 +0200

    [Plugin] fix: add missing file

---
+++ openmole/plugins/org.openmole.plugin.method.sensitivity/src/main/scala/org/openmole/plugin/method/sensitivity/MorrisHook.scala
@@ -0,0 +1,27 @@
+package org.openmole.plugin.method.sensitivity
+
+import org.openmole.core.dsl.File
+import org.openmole.core.dsl.extension._
+import org.openmole.core.exception.UserBadDataError
+
+object MorrisHook {
+
+  def apply(method: Sensitivity.MorrisMethodContainer, dir: FromContext[File])(implicit name: sourcecode.Name, definitionScope: DefinitionScope) =
+    Hook("MorrisHook") { p ⇒
+      import p._
+
+      val inputs = ScalarOrSequenceOfDouble.prototypes(method.inputs)
+
+      val muFile = dir / "mu.csv"
+      Sensitivity.writeFile(muFile.from(context), inputs, method.outputs, Morris.mu(_, _)).from(context)
+
+      val muStarFile = dir / "muStar.csv"
+      Sensitivity.writeFile(muStarFile.from(context), inputs, method.outputs, Morris.muStar(_, _)).from(context)
+
+      val sigmaFile = dir / "sigma.csv"
+      Sensitivity.writeFile(sigmaFile.from(context), inputs, method.outputs, Morris.sigma(_, _)).from(context)
+
+      context
+    }
+
+}
