3c8164bdb5c08af2469a056e31c771a55cd3815a
Romain Reuillon <romain.reuillon@iscpif.fr>
2019-05-26 16:41:54 +0200

    [Doc] fix: add empty div

--- openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/Menu.scala
+++ openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/Menu.scala
@@ -29,6 +29,8 @@ object Menu {
   val inputStyle = paddingTop := 15
   val navId = "omwesite"
 
+  def banner(sitePage: SitePage) = div
+
   //  def banner(sitePage: SitePage) = match {
   //    case cp: ContentPage ⇒ div(
   //      marginTop := 10,
@@ -48,6 +50,7 @@ object Menu {
 
   def build(sitePage: SitePage) = {
     div(classIs("container"))(
+      banner(sitePage),
       tags2.nav(navClass, paddingRight := 20)(
         div(classIs(container_fluid))(
           div(classIs(navbar_header))(
