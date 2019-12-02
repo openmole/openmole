a090a984cb905a00cbf3b17133228e0300ff8ad5
Romain Reuillon <romain.reuillon@iscpif.fr>
2019-12-02 20:32:38 +0100

    [Build] fix: mgo version

--- openmole/project/Libraries.scala
+++ openmole/project/Libraries.scala
@@ -9,7 +9,7 @@ object Libraries {
   lazy val gridscaleVersion = "2.27"
   lazy val sshjVersion = "0.27.0"
   lazy val containerVersion = "1.4"
-  lazy val mgoVersion = "3.37"
+  lazy val mgoVersion = "3.38"
   lazy val bouncyCastleVersion = "1.60"
   lazy val d3Version = "3.5.12"
   lazy val tooltipserVersion = "3.3.0"
