269531aabadb75a76db041a214f75375dd1bd108
Mathieu <mathieu.leclaire@iscpif.fr>
2020-08-11 14:22:48 +0200

    Plot temporal series in Line mode

--- openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/plot/Tools.scala
+++ openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/plot/Tools.scala
@@ -1,5 +1,8 @@
 package org.openmole.gui.client.tool.plot
+
 import scala.util.Try
+
+import scala.scalajs.js.RegExp
 
 object Tools {
 
@@ -8,5 +11,17 @@ object Tools {
       _.toDouble
     }
   ).toOption.getOrElse(Array(0.0))
+
+  val arrayRegEx = new RegExp("\\[[0-9].*,*\\]")
+
+  def isDataArray(value: String) = {
+    arrayRegEx.test(value)
+  }
+
+  def getDataArrays(dataCol: Seq[String]) = {
+    dataCol.map { d ⇒
+      arrayRegEx.exec(d).map { _.get }.head.tail.dropRight(1).split(',').toSeq
+    }
+  }
 
 }
