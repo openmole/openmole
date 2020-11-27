86dd49f6f63e4989dc9e91756b0704eead910859
Romain Reuillon <romain.reuillon@iscpif.fr>
2020-11-27 18:07:52 +0100

    [Core] enh: set name

--- openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
+++ openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
@@ -21,7 +21,7 @@ import java.util.{ Calendar, Locale }
 
 package object buildinfo {
 
-  def name = "P... P..."
+  def name = "Shooting Star"
 
   case class Version(value: String, name: String, time: Long) {
     override def toString = value
