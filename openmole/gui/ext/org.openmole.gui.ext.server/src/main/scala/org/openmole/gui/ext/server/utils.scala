a4517002e234f3855773bf9624cb5ea97e15dd95
Romain Reuillon <romain.reuillon@iscpif.fr>
2020-12-09 11:06:37 +0100

    [GUI] fix: add missing routes

--- openmole/gui/ext/org.openmole.gui.ext.server/src/main/scala/org/openmole/gui/ext/server/utils.scala
+++ openmole/gui/ext/org.openmole.gui.ext.server/src/main/scala/org/openmole/gui/ext/server/utils.scala
@@ -268,7 +268,6 @@ object utils {
 
   val openmoleGrammarName = "openmole_grammar_template.js"
   val openmoleGrammarMode = "mode-openmole.js"
-
   val openmoleThemeName = "theme-github.js"
 
   def updateIfChanged(file: File)(update: File ⇒ Unit)(implicit fileService: FileService, newFile: TmpDirectory) = {
