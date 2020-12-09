1f6faea5e8a5e3f4070f1839bd3eb5df793b792a
Mathieu <mathieu.leclaire@iscpif.fr>
2020-12-09 17:12:20 +0100

    Revert "[GUI] Fix OpenMOLE theme"
    This reverts commit 1dda04a1f9ee1f4a6dd21b7044cf1761d89d64fe.

--- openmole/gui/ext/org.openmole.gui.ext.server/src/main/scala/org/openmole/gui/ext/server/utils.scala
+++ openmole/gui/ext/org.openmole.gui.ext.server/src/main/scala/org/openmole/gui/ext/server/utils.scala
@@ -265,11 +265,8 @@ object utils {
 
   val openmoleFileName = "openmole.js"
   val depsFileName = "deps.js"
-
   val openmoleGrammarName = "openmole_grammar_template.js"
   val openmoleGrammarMode = "mode-openmole.js"
-
-  val openmoleThemeName = "theme-openmole.js"
 
   def updateIfChanged(file: File)(update: File ⇒ Unit)(implicit fileService: FileService, newFile: TmpDirectory) = {
     import org.openmole.core.fileservice._
