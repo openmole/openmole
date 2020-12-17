9671b6990aff0789082e1b77cde6f767660b1dfe
Romain Reuillon <romain.reuillon@iscpif.fr>
2020-12-17 13:11:54 +0100

    Add push script for containers

---
+++ containers/push.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+if (( $# != 2 )); then
+  echo "Please specify the container name and the version"
+  exit 1
+fi
+
+NAME=$1
+VERSION=$2
+
+cd $NAME/$VERSION && \
+docker build . -t openmole/$NAME:$VERSION && \
+docker push openmole/$NAME:$VERSION &&
+docker rmi openmole/$NAME:$VERSION
+
