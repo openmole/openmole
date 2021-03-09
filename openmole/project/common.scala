1d96d8995922278ef49d8458b6916de20761c024
Romain Reuillon <romain.reuillon@iscpif.fr>
2021-03-09 18:22:40 +0100

    [Lib] update gridscale

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -4,11 +4,11 @@ package openmole
 object common {
   def scalaVersionValue = "2.13.5"
 
-  def gridscaleVersion = "2.34"
+  def gridscaleVersion = "2.35"
   def mgoVersion = "3.47"
-  def sshjVersion = "0.29.0"
+  def sshjVersion = "0.31.0"
   def containerVersion = "1.15"
-  def bouncyCastleVersion = "1.65"
+  def bouncyCastleVersion = "1.68"
   def netLogo5Version = "5.3.1"
   def netLogo6Version = "6.2.0"
   def scalaTagsVersion = "0.9.1"
