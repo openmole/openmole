ea900dd4b676261ac64d86e776729ab0db12b9ed
Romain Reuillon <romain.reuillon@iscpif.fr>
2021-03-26 09:56:43 +0100

    [Core] enh: reduce memory footprint of localexecution job

--- openmole/core/org.openmole.core.workflow/src/main/scala/org/openmole/core/workflow/execution/LocalExecutionJob.scala
+++ openmole/core/org.openmole.core.workflow/src/main/scala/org/openmole/core/workflow/execution/LocalExecutionJob.scala
@@ -10,7 +10,19 @@ import org.openmole.core.workflow.task.TaskExecutionContext
  * @param executionContext
  * @param moleExecution
  */
-case class LocalExecutionJob(executionContext: TaskExecutionContext, jobs: Iterable[MoleJob], moleExecution: Option[MoleExecution]) extends ExecutionJob {
+object LocalExecutionJob {
+  def apply(
+    executionContext: TaskExecutionContext,
+    jobs:             Iterable[MoleJob],
+    moleExecution:    Option[MoleExecution]) =
+    new LocalExecutionJob(
+      executionContext = executionContext,
+      jobs = jobs.toArray,
+      _moleExecution = moleExecution.getOrElse(null))
+}
+
+case class LocalExecutionJob(executionContext: TaskExecutionContext, jobs: Array[MoleJob], _moleExecution: MoleExecution) extends ExecutionJob {
   def moleJobIds = jobs.map(_.id)
   def environment = executionContext.localEnvironment
+  def moleExecution: Option[MoleExecution] = Option(_moleExecution)
 }
