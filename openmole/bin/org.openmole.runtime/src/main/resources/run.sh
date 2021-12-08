1281cba9ac3fb57b2b9eba44172937e74e25978c
Romain Reuillon <romain.reuillon@iscpif.fr>
2021-12-08 12:19:54 +0100

    [App] fix: make it work on jdk 17

--- openmole/bin/org.openmole.runtime/src/main/resources/run.sh
+++ openmole/bin/org.openmole.runtime/src/main/resources/run.sh
@@ -68,6 +68,7 @@ fi
 
 java -Djava.io.tmpdir="${TMPDIR}" -Dsun.jnu.encoding=UTF-8 -Dfile.encoding=UTF-8 -Duser.country=US -Duser.language=en -Xss2M -Xms64m -Xmx${MEMORY} -Dosgi.locking=none -Dosgi.configuration.area="${OSGI_CONFIGDIR}" $FLAG -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -XX:CompressedClassSpaceSize=128m \
   -XX:+UseG1GC -XX:ParallelGCThreads=1 -XX:CICompilerCount=2 -XX:ConcGCThreads=1 -XX:G1ConcRefinementThreads=1 -XX:+UseStringDeduplication \
+  --add-opens java.base/java.lang.invoke=ALL-UNNAMED \
   -cp "${LOCATION}/launcher/*" org.openmole.launcher.Launcher --plugins "${LOCATION}/plugins/" --priority "logging" --run org.openmole.runtime.SimExplorer --osgi-directory "${OSGI_CONFIGDIR}" -- --workspace "${OPENMOLE_WORKSPACE}" $@
 
 RETURNCODE=$?
