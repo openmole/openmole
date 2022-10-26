d0057af4761c9e254ab5307dcea5c68878482fc5
Romain Reuillon <romain.reuillon@iscpif.fr>
2022-10-26 15:47:43 +0200

    [Plugin] enh: simplify method data constructor

--- openmole/plugins/org.openmole.plugin.tool.methoddata/src/main/scala/org/openmole/plugin/tool/methoddata/MethodData.scala
+++ openmole/plugins/org.openmole.plugin.tool.methoddata/src/main/scala/org/openmole/plugin/tool/methoddata/MethodData.scala
@@ -2,15 +2,5 @@ package org.openmole.plugin.tool.methoddata
 
 import io.circe.*
 
-object MethodData {
-  def apply[T: Encoder](_name: T ⇒ String): MethodData[T] = new MethodData[T] {
-    override def name(t: T): String = _name(t)
-    override def encoder = summon[Encoder[T]]
-  }
-}
-
-trait MethodData[T] {
-  def name(t: T): String
-  def encoder: Encoder[T]
-}
+case class MethodData[T](name: T => String)(using val encoder: Encoder[T])
 
