cf3f53cf26e654fa69d751eb63c247e6bd638895
Romain Reuillon <romain.reuillon@iscpif.fr>
2023-07-11 14:55:31 +0200

    [Core] enh: add missing file

---
+++ openmole/core/org.openmole.core.setter/src/main/scala/org/openmole/core/setter/DefinitionScope.scala
@@ -0,0 +1,30 @@
+package org.openmole.core.setter
+
+/*
+ * Copyright (C) 2023 Romain Reuillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+object DefinitionScope:
+  case class Internal(name: String) extends DefinitionScope
+  case object User extends DefinitionScope
+
+  given Conversion[String, Internal] = scope => Internal(scope)
+
+  object user:
+    implicit def default: DefinitionScope = User
+
+sealed trait DefinitionScope
