3543553027d5ab1a27dd630324b97bdf422f7f96
Romain Reuillon <romain.reuillon@iscpif.fr>
2023-11-30 22:18:30 +0100

    [Core] fix: don't prefix Any

--- openmole/third-parties/org.openmole.tool.types/src/main/scala/org/openmole/tool/types/TypeTool.scala
+++ openmole/third-parties/org.openmole.tool.types/src/main/scala/org/openmole/tool/types/TypeTool.scala
@@ -161,7 +161,7 @@ object TypeTool {
       if rc.isArray
       then s"Array[${manifestToString(m.typeArguments.head)}]"
       else
-        if rc.isPrimitive
+        if rc.isPrimitive || rc == classOf[Any]
         then m.toString
         else s"_root_.${m.toString()}"
 
