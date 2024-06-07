ae8cb4ef0e8bc4e79d04363ac78c37edacba9a07
Mathieu <mathieu.leclaire@iscpif.fr>
2024-06-07 14:59:02 +0200

    Initiate omr metadata

--- openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/files/OMRContent.scala
+++ openmole/gui/client/org.openmole.gui.client.core/src/main/scala/org/openmole/gui/client/core/files/OMRContent.scala
@@ -3,7 +3,7 @@ package org.openmole.gui.client.core.files
 import org.openmole.gui.shared.data.*
 import scaladget.bootstrapnative.bsn.*
 import com.raquo.laminar.api.L.*
-import org.openmole.gui.client.core.files.PlotContent.PlotContentSection
+import org.openmole.gui.client.core.files.PlotContent.{OMRMetadata, PlotContentSection}
 import org.openmole.gui.client.core.{CoreFetch, Panels}
 import org.openmole.gui.client.core.files.TabContent.TabData
 import org.openmole.gui.client.ext.*
@@ -11,8 +11,12 @@ import org.openmole.gui.shared.api.*
 
 object OMRContent:
 
-  def buildTab(safePath: SafePath, sections: Seq[GUIOMRSectionContent])(using panels: Panels, api: ServerAPI, basePath: BasePath, guiPlugins: GUIPlugins) =
-    val (rowData: RowData, rawContent: String) = ResultData.fromOMR(sections)
-    val pcSections = sections.map: s =>
+  def buildTab(safePath: SafePath, guiOMRContent: GUIOMRContent)(using panels: Panels, api: ServerAPI, basePath: BasePath, guiPlugins: GUIPlugins) =
+    val (rowData: RowData, rawContent: String) = ResultData.fromOMR(guiOMRContent.section)
+    val pcSections = guiOMRContent.section.map: s =>
       PlotContentSection(s.name.getOrElse("section"), rawContent, rowData, "initialHash")
-    PlotContent.buildTab(safePath, FileContentType.OpenMOLEResult, pcSections)
+    val scriptText =
+      guiOMRContent.script match
+        case Some(gos: GUIOMRScript)=> s"""${gos.`import`.map(_.content + "\n")} \n\n ${gos.content}"""
+        case _=> "Scrit unvailable"
+    PlotContent.buildTab(safePath, FileContentType.OpenMOLEResult, pcSections, Some(OMRMetadata(scriptText, guiOMRContent.openMoleVersion)))
