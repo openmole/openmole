a44f4b8b7ce4b5c6ca47c684800cacaa36e2064b
Sébastien Plutniak <53480606+sebastien-plutniak@users.noreply.github.com>
2025-02-17 18:58:51 +0100

    Update HDOSE.scala
    Quelques corrections, des simplifications des formulations, et une suggestion:
    > objectives should be under given thresholds or close to given values
    Sachant qu'on peut aussi utiliser `delta`, ce qui est bien pratique.

--- openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/content/explore/HDOSE.scala
+++ openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/content/explore/HDOSE.scala
@@ -23,15 +23,15 @@ object HDOSE extends PageContent(html"""
 
 ${h2{"HDOSE description"}}
 
-The High Dimension Origin Space Exploration (HDOSE) method is used to ${b{"explore the multiples antecedents of a pattern"}} when the input space is of high dimension (i.e. > 6). It produces input parameter values that produce a given pattern described by a set of constraints on the objectives (i.e. objectives should be under given thresholds). HDOSE optimize the fitness and when it finds solutions that are good enough it keep them and archive. New solutions are added to the archive when there input values are distant of at least a distance ${i("d = 1.0")} of all existing solution in the archive (the distance L1 is used). When the archive size reaches 500 a new value for ${i("d")} is computed to shrink the archive size under 500 given that all the solutions present in the archive are distant of at least ${i("d")}.
+The High Dimension Origin Space Exploration (HDOSE) method is used to ${b{"explore the multiple antecedents of a pattern"}} when the input space is of high dimension (i.e. > 6). It generates input parameter values that produce a pattern described by a set of constraints on the objectives (i.e. objectives should be under given thresholds or close to given values). HDOSE optimize the fitness and keeps and archives the solutions that are good enough. New solutions are added to the archive when their input values are distant of at least a distance ${i("d = 1.0")} of all existing solution in the archive (the distance L1 is used). When the archive size reaches 500 a new value for ${i("d")} is computed to shrink the archive size under 500 given that all the solutions present in the archive are distant of at least ${i("d")}.
 
 
-The hook arguments for the ${code{"OSEEvolution"}} are:
+The hook arguments for the ${code{"HDOSEEvolution"}} are:
 ${Evolution.hookOptions}
 
 ${h3{"Example"}}
 
-Here is a use example of the OSE method in an OpenMOLE script:
+This is an example of the HDOSE method in an OpenMOLE script:
 
 $br$br
 
@@ -64,10 +64,10 @@ HDOSEEvolution(
 """, name = "HDOSE", header = "val modelTask = EmptyTask()")}
 
 
-${i{"origin"}} describes the genome of the optimisation. To compute the distance, each part of the genome are normalised. If you want to give more weight to a given part of the genome in the distance computation you can use the weight keyword.
+${i{"origin"}} describes the genome of the optimisation. To compute the distance, each part of the genome are normalised. To give more weight to a given part of the genome in the distance computation, use the `weight` keyword.
 
 $br
 
-${i{"objective"}} describe the pattern to reach with inequalities. The patten is considered as reached when all the objective are under their threshold value. In this example HDOSE computes a maximal diversity of inputs for which all the outputs are under their respective threshold values.
+${i{"objective"}} use inequalities to define the pattern to reach. The pattern is reached when all the objectives are under their threshold value. In this example, HDOSE computes a maximal diversity of inputs for which all the outputs are under their respective threshold value.
 
 """)
