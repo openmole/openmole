73e59f2220baa8be60cdc5b3f541141c4b4b9c2c
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-03-18 14:16:09 +0100

    [GUI] fix: section display

--- openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/Component.scala
+++ openmole/gui/client/org.openmole.gui.client.tool/src/main/scala/org/openmole/gui/client/tool/Component.scala
@@ -3,6 +3,8 @@ package org.openmole.gui.client.tool
 import com.raquo.laminar.api.L
 import com.raquo.laminar.api.L.*
 import com.raquo.laminar.nodes.ReactiveElement.isActive
+import com.raquo.laminar.api.features.unitArrows
+import scaladget.bootstrapnative.bsn.{btn_secondary_string, btn_success_string}
 
 object Component:
 
@@ -24,3 +26,33 @@ object Component:
         ),
         checked.toObservable --> Observer[Boolean](onChange)
       )
+
+
+  object ExclusiveButtons:
+    def apply(
+     buttons: Seq[String],
+     activeStateClass: String = btn_success_string,
+     unactiveStateClass: String = btn_secondary_string,
+     initial: Int = 0) = new ExclusiveButtons(buttons, activeStateClass, unactiveStateClass, Var(initial))
+
+  case class ExclusiveButtons(buttons: Seq[String], activeStateClass: String, unactiveStateClass: String, selected: Var[Int]):
+
+    import scaladget.bootstrapnative.*
+
+    lazy val element =
+      div(
+        bsnsheet.btnGroup, bsnsheet.btnGroupToggle, dataAttr("toggle") := "buttons",
+        children <--
+          selected.signal.distinct.map: s =>
+            buttons.zipWithIndex.map: (rb, index) =>
+              val isSelected = s == index
+
+              label(
+                cls := s"btn ${if isSelected then activeStateClass else  unactiveStateClass}",
+                cls.toggle("focus active") := isSelected,
+                input(`type` := "radio"/*, name := "options"*/, idAttr := s"option${index + 1}", checked := isSelected),
+                rb.text,
+                onClick --> selected.set(index)
+              )
+      )
+
