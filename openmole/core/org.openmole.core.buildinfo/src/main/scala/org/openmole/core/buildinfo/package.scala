d6e3b283c310ee33df661f578624ffdb97082290
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-06-18 21:58:24 +0200

    Set version name

--- openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
+++ openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
@@ -21,7 +21,7 @@ import java.util.{ Calendar, Locale }
 
 package object buildinfo:
 
-  def name = "A... A..."
+  def name = "Almost Amazing"
 
   case class Version(value: String, name: String, time: Long):
     override def toString = value
