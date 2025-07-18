20ecccb7542edaa770aa0ec535aa8d1574f42c68
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-07-18 18:04:38 +0200

    [Core] enh: remove some useless ref to global executor

--- openmole/core/org.openmole.core.compiler/src/main/scala/org/openmole/core/compiler/Interpreter.scala
+++ openmole/core/org.openmole.core.compiler/src/main/scala/org/openmole/core/compiler/Interpreter.scala
@@ -32,7 +32,7 @@ import scala.annotation.tailrec
 //import scala.tools.nsc._
 //import scala.tools.nsc.reporters._
 import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
+//import scala.concurrent.ExecutionContext.Implicits.global
 //import scala.tools.nsc.interpreter._
 import org.openmole.core.fileservice.FileService
 import org.openmole.core.workspace.TmpDirectory
