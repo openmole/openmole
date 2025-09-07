849de3ee9fff2e501a5eb8cc2801bbcb4985c1e1
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-09-07 13:40:42 +0200

    [Build] enh: update sbt and scala.js

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -9,7 +9,7 @@ object common {
   def sjs(name: String) = name + "_sjs1"
 
   def scala3VersionValue = "3.7.2"
-  def scalajsVersion = "1.19.0"
+  def scalajsVersion = "1.20.1"
 
   def scalaXMLVersion = "2.1.0"
   def gridscaleVersion = "2.62"
