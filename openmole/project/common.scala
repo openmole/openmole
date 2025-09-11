de11151ecb8f9a3baefa8f338256764871f8b9ff
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-09-11 12:55:11 +0200

    Revert "[Build] enh: update scala"
    This reverts commit e63369cd5f8a183e368beee1fb67bb120b5e7594.

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -8,7 +8,7 @@ object common {
 
   def sjs(name: String) = name + "_sjs1"
 
-  def scala3VersionValue = "3.7.3"
+  def scala3VersionValue = "3.7.2"
   def scalajsVersion = "1.20.1"
 
   def scalaXMLVersion = "2.1.0"
