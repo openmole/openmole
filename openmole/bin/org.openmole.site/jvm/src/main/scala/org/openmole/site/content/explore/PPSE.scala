45fbb4be5271eeaa28bfa0512561abd4f17e661e
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-11-08 15:29:44 +0100

    [Site] enh: add ppse

--- openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/content/explore/PPSE.scala
+++ openmole/bin/org.openmole.site/jvm/src/main/scala/org/openmole/site/content/explore/PPSE.scala
@@ -49,5 +49,35 @@ PPSEEvolution(
     output2 in (0.0 to 4000.0 by 50.0))
 ) hook (workDirectory / "results", frequency = 100)
 """, name = "PPSE", header = "val modelTask = EmptyTask()")}
+Optionally you can define density distribution on you inputs:
+
+$br$br
+
+${hl.openmole("""
+// Seed declaration for random number generation
+val myseed = Val[Int]
+
+val param1 = Val[Double]
+val param2 = Val[Double]
+val output1 = Val[Double]
+val output2 = Val[Double]
+
+// PSE method
+PPSEEvolution(
+  evaluation = modelTask,
+  parallelism = 10,
+  termination = 100,
+  genome = Seq(
+    param1  in (0.0 to 1.0),
+    param2 in (-10.0 to 10.0)),
+  objective = Seq(
+    output1 in (0.0 to 40.0 by 5.0),
+    output2 in (0.0 to 4000.0 by 50.0)),
+  density = Seq(
+    param1 in NormalDistribution(2.0, 0.1),
+    humanInformProbability in NormalDistribution(0.0, 1.0)
+  )
+) hook (workDirectory / "results", frequency = 100)
+""", name = "PPSE density", header = "val modelTask = EmptyTask()")}
 
 """)
