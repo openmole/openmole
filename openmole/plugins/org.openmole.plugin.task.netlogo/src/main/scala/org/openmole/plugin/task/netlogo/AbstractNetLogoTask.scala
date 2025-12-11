460fea7b39233802fc2bf11a36cb23e66020d6bb
Romain Reuillon <romain.reuillon@iscpif.fr>
2025-12-11 22:01:46 +0100

    [Core] enh: improve error message in case of dimension error

--- openmole/plugins/org.openmole.plugin.task.netlogo/src/main/scala/org/openmole/plugin/task/netlogo/AbstractNetLogoTask.scala
+++ openmole/plugins/org.openmole.plugin.task.netlogo/src/main/scala/org/openmole/plugin/task/netlogo/AbstractNetLogoTask.scala
@@ -206,10 +206,14 @@ object AbstractNetLogoTask:
       if !runtimeClass.isArray
       then Variable.unsecureUntyped(mapped.v, AbstractNetLogoTask.cast(outputValue, runtimeClass))
       else
+        println("fetch array")
         val netLogoCollection: util.AbstractCollection[Any] =
-          if (classOf[AbstractCollection[Any]].isAssignableFrom(outputValue.getClass))
-          then outputValue.asInstanceOf[AbstractCollection[Any]]
+          if classOf[AbstractCollection[Any]].isAssignableFrom(outputValue.getClass)
+          then
+            println("cast array")
+            outputValue.asInstanceOf[AbstractCollection[Any]]
           else
+            println("not cast")
             val newArray = new util.LinkedList[Any]()
             newArray.add(outputValue)
             newArray
