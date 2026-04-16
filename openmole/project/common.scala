b5a0a2f3f46efeb049a7067e143452d3ec4d03f6
Romain Reuillon <romain.reuillon@iscpif.fr>
2026-04-16 20:36:41 +0200

    [Lib] fix: update mgo

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -14,7 +14,7 @@ object common {
 
   def scalaXMLVersion = "2.4.0"
   def gridscaleVersion = CommitVersion("f6bcbcf097", "2.63")
-  def mgoVersion = CommitVersion("466a92dd38", "3.68")
+  def mgoVersion = CommitVersion("5630b77eac", "3.68")
   def sshjVersion = "0.40.0"
   def containerVersion = CommitVersion("669efd4da2", "1.33")
   def bouncyCastleVersion = "1.82"
