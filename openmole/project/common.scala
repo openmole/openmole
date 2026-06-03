15e7b93b7eb20635f9920f1467e6b83907c176e4
Romain Reuillon <romain.reuillon@iscpif.fr>
2026-06-03 12:46:40 +0200

    Revert "[Lib] enh: update mgo"
    This reverts commit 62f269023a464cd76dc19d16641731716b2cdb34.

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -14,7 +14,7 @@ object common {
 
   def scalaXMLVersion = "2.4.0"
   def gridscaleVersion = CommitVersion("f6bcbcf097", "2.63")
-  def mgoVersion = CommitVersion("e3506e264a", "3.68")
+  def mgoVersion = CommitVersion("811dd1d65d", "3.68")
   def sshjVersion = "0.40.0"
   def containerVersion = CommitVersion("669efd4da2", "1.33")
   def bouncyCastleVersion = "1.82"
