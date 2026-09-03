dd3cab215c1addd74e73d39f475342703ffcd01f
Romain Reuillon <romain.reuillon@iscpif.fr>
2026-09-03 14:39:52 +0200

    [Build] enh: update scala

--- openmole/project/common.scala
+++ openmole/project/common.scala
@@ -9,7 +9,7 @@ object common {
 
   def sjs(name: String) = name + "_sjs1"
 
-  def scala3VersionValue = "3.9.0-RC6"
+  def scala3VersionValue = "3.9.0"
   def scalajsVersion = "1.22.0"
 
   def scalaXMLVersion = "2.4.0"
