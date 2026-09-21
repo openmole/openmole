dceb5e648da7f22ced865c7a1cd6075890122a69
Romain Reuillon <romain.reuillon@iscpif.fr>
2026-09-21 13:53:53 +0200

    [Build] enh: set name

--- openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
+++ openmole/core/org.openmole.core.buildinfo/src/main/scala/org/openmole/core/buildinfo/package.scala
@@ -21,7 +21,7 @@ import java.util.{ Calendar, Locale }
 
 package object buildinfo:
 
-  def name = "C... C..."
+  def name = "Deliberate Drift"
 
   case class Version(value: String, name: String, time: Long):
     override def toString = value
